d84a073c4029c96846ca351a12f7417359fa0190
twobiers <22715034+twobiers@users.noreply.github.com>
2024-01-01 19:02:38 +0100

    remove compact strings

--- calculate_average_twobiers.sh
+++ calculate_average_twobiers.sh
@@ -15,5 +15,5 @@
 #  limitations under the License.
 #
 
-JAVA_OPTS="-XX:+UseStringDeduplication -XX:+CompactStrings -XX:+UseTransparentHugePages -da"
+JAVA_OPTS="-XX:+UseStringDeduplication -XX:+UseTransparentHugePages -da"
 java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_twobiers
