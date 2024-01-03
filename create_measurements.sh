c0bfda15478feb02f8b65594d1cdd4494a0c3fa4
datdenkikniet <jcdra1@gmail.com>
2024-01-03 17:28:31 +0000

    Make seed configurable

--- create_measurements.sh
+++ create_measurements.sh
@@ -16,4 +16,4 @@
 #
 
 
-java --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CreateMeasurements $1
+java --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CreateMeasurements $1 $2
