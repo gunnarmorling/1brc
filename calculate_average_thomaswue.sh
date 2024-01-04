06974f4a3b3ac8feb63ea9399c3ef15610d8cfc0
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2024-01-04 22:04:11 +0100

    Adjust script to be more quiet.

--- calculate_average_thomaswue.sh
+++ calculate_average_thomaswue.sh
@@ -15,41 +15,43 @@
 #  limitations under the License.
 #
 
-sdk use java 21.0.1-graal
-echo "Script executed from: ${PWD}"
 NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -march=native"
 JAVA_OPTS=""
 
 if [ -z "${JVM_MODE}" ]; then
-    echo "Chosing native image mode, set JVM_MODE variable to select JVM mode."
+    # Chosing native image mode, set JVM_MODE variable to select JVM mode.
     if [ -z "${PGO_MODE}" ]; then
         if [ -f ./image_calculateaverage_thomaswue ]; then
-            echo 'Picking up existing native image.'
+            : # Picking up existing native image.
         else
             echo 'Using native image without PGO data, set PGO_MODE to use profile-guided optimizations instead.'
+            sdk use java 21.0.1-graal
             native-image $NATIVE_IMAGE_OPTS -cp target/average-1.0.0-SNAPSHOT.jar -o image_calculateaverage_thomaswue dev.morling.onebrc.CalculateAverage_thomaswue
         fi
         time ./image_calculateaverage_thomaswue
     else
         if [ -f ./image_calculateaverage_thomaswue_pgo ]; then
-            echo 'Picking up existing native image with PGO.'
+            : # Picking up existing native image with PGO.
 	else
             if [ -f ./profile_thomaswue.iprof ]; then
                 echo 'Picking up profiling information from profile_thomaswue.iprof.'
             else
                 echo 'Could not find profiling information, therefore it will be now regenerated.'
+                sdk use java 21.0.1-graal
                 native-image --pgo-instrument -cp target/average-1.0.0-SNAPSHOT.jar -o instrumented_calculateaverage_thomaswue dev.morling.onebrc.CalculateAverage_thomaswue
                 echo 'Running example with instrumented image for 20 seconds.'
                 timeout 20 ./instrumented_calculateaverage_thomaswue
                 mv default.iprof profile_thomaswue.iprof
             fi
             echo 'Could not find a native image, building a new one now.'
+            sdk use java 21.0.1-graal
             native-image $NATIVE_IMAGE_OPTS --pgo=profile_thomaswue.iprof -cp target/average-1.0.0-SNAPSHOT.jar -o image_calculateaverage_thomaswue_pgo dev.morling.onebrc.CalculateAverage_thomaswue
 	fi
         time ./image_calculateaverage_thomaswue_pgo
     fi
 else
-    echo "Chosing JVM mode, unset JVM_MODE variable to select native image mode."
+    # Chosing JVM mode, unset JVM_MODE variable to select native image mode.
+    sdk use java 21.0.1-graal
     time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_thomaswue
 fi
 
