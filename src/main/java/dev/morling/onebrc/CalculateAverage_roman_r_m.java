679a58fabcf436b60f4128d21742c85f8f4df353
Roman Musin <995612+roman-r-m@users.noreply.github.com>
2024-01-04 22:45:38 +0000

    MMap + custom double parsing ~ 1:30 (down from ~ 2:05)

--- src/main/java/dev/morling/onebrc/CalculateAverage_roman_r_m.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_roman_r_m.java
@@ -15,14 +15,115 @@
  */
 package dev.morling.onebrc;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
 import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
 import java.util.TreeMap;
 
 public class CalculateAverage_roman_r_m {
 
     private static final String FILE = "./measurements.txt";
+
+    public static void main(String[] args) throws IOException {
+        long fileSize = new File(FILE).length();
+
+        var measurements = new TreeMap<String, ResultRow>();
+
+        var station = new ByteString();
+        var value = new ByteString();
+        var parsingStation = true;
+
+        long chunkSize = 1024 * 1024 * 1024;
+        long offset = 0;
+        var channel = FileChannel.open(Paths.get(FILE));
+        MappedByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, offset, chunkSize);
+        while (offset < fileSize) {
+            if (buf == null || !buf.hasRemaining()) {
+                buf = channel.map(FileChannel.MapMode.READ_ONLY, offset, Math.min(chunkSize, fileSize - offset));
+            }
+            byte c = buf.get();
+            offset++;
+            if (c == ';') {
+                value.reset();
+                parsingStation = false;
+            }
+            else if (c == '\r') {
+            }
+            else if (c == '\n') {
+                String name = station.toString();
+                double val = value.parseDouble();
+
+                var a = measurements.computeIfAbsent(name, x -> new ResultRow());
+                a.min = Math.min(a.min, val);
+                a.max = Math.max(a.max, val);
+                a.sum += val;
+                a.count++;
+
+                parsingStation = true;
+                station.reset();
+
+            }
+            else if (parsingStation) {
+                station.append(c);
+            }
+            else {
+                value.append(c);
+            }
+        }
+
+        // var reader = new BufferedReader(new FileReader(FILE), 1024 * 1024);
+        // String l;
+        // while ((l = reader.readLine()) != null) {
+        // int i = l.indexOf(';');
+        // String station = l.substring(0, i);
+        // double value = Double.parseDouble(l.substring(i + 1));
+        //
+        // var a = measurements.computeIfAbsent(station, _ -> new ResultRow());
+        //
+        // a.min = Math.min(a.min, value);
+        // a.max = Math.max(a.max, value);
+        // a.sum += value;
+        // a.count++;
+        // }
+
+        System.out.println(measurements);
+    }
+
+    static final class ByteString {
+
+        private byte[] buf = new byte[128];
+        private int len = 0;
+
+        void append(byte b) {
+            if (buf.length == len) {
+                var newBuf = new byte[(int) (buf.length * 1.5)];
+                System.arraycopy(this.buf, 0, newBuf, 0, len);
+                this.buf = newBuf;
+            }
+            buf[len++] = b;
+        }
+
+        void reset() {
+            len = 0;
+        }
+
+        @Override
+        public String toString() {
+            return new String(buf, 0, len);
+        }
+
+        public double parseDouble() {
+            double res = 0;
+            boolean negate = buf[0] == '-';
+            for (int i = negate ? 1 : 0; i < len - 2; i++) {
+                res = res * 10 + (buf[i] - '0');
+            }
+            res = res + (buf[len - 1] - '0') / 10.0;
+            return negate ? -res : res;
+        }
+    }
 
     private static final class ResultRow {
         double min = 0.0;
@@ -37,25 +138,5 @@ public class CalculateAverage_roman_r_m {
         private double round(double value) {
             return Math.round(value * 10.0) / 10.0;
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-        var measurements = new TreeMap<String, ResultRow>();
-        var reader = new BufferedReader(new FileReader(FILE), 1024 * 1024);
-        String l;
-        while ((l = reader.readLine()) != null) {
-            int i = l.indexOf(';');
-            String station = l.substring(0, i);
-            double value = Double.parseDouble(l.substring(i + 1));
-
-            var a = measurements.computeIfAbsent(station, _ -> new ResultRow());
-
-            a.min = Math.min(a.min, value);
-            a.max = Math.max(a.max, value);
-            a.sum += value;
-            a.count++;
-        }
-
-        System.out.println(measurements);
     }
 }
