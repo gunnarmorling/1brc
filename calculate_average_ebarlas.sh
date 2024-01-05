0a7ae676319ddf214d6b49098e72dadfef79b186
Elliot Barlas <elliotbarlas@gmail.com>
2024-01-05 07:08:31 -0800

    Implement imperative state machine for floating point parser rather then generic, adaptive loop.

--- calculate_average_ebarlas.sh
+++ calculate_average_ebarlas.sh
@@ -15,6 +15,7 @@
 #  limitations under the License.
 #
 
+source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 21.0.1-graalce
 JAVA_OPTS=""
 time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_ebarlas measurements.txt 8
