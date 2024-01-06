d998a604dc3aedc2f5a56458a7ba20e89fef7dbd
netrunnereve <139727413+netrunnereve@users.noreply.github.com>
2024-01-05 23:33:30 -0500

    begin mmap work

--- src/main/java/dev/morling/onebrc/CalculateAverage_netrunnereve.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_netrunnereve.java
@@ -16,10 +16,11 @@
 package dev.morling.onebrc;
 
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Arrays;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 
 public class CalculateAverage_netrunnereve {
 
@@ -32,59 +33,77 @@ public class CalculateAverage_netrunnereve {
         private int count = 0;
     }
 
-    public static void main() {
+    public static void main(String[] args) {
         try {
-            BufferedReader filBuf = new BufferedReader(new FileReader(FILE));
-            String line = filBuf.readLine();
+            RandomAccessFile mraf = new RandomAccessFile(FILE, "r");
+            MappedByteBuffer mbuf = mraf.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, mraf.getChannel().size());
 
-            HashMap<String, MeasurementAggregator> staHash = new HashMap<String, MeasurementAggregator>();
-
-            while (line != null) {
-                String[] linSpl = line.split(";", 2); // station, measurement
-                String station = linSpl[0];
-
-                MeasurementAggregator ma = staHash.get(station);
-                if (ma == null) {
-                    ma = new MeasurementAggregator();
-                    staHash.put(station, ma);
-                }
-
-                short tempa = Short.parseShort(linSpl[1].replace(".", "")); // x10
-                if (tempa < ma.min) {
-                    ma.min = tempa;
-                }
-                if (tempa > ma.max) {
-                    ma.max = tempa;
-                }
-                ma.sum += tempa;
-                ma.count++;
-
-                line = filBuf.readLine();
-            }
-
-            String[] staArr = new String[staHash.size()];
-            int j = 0;
-            for (String i : staHash.keySet()) {
-                staArr[j] = i;
-                j++;
-            }
-            Arrays.sort(staArr);
-
-            String out = "{";
-            for (int i = 0; i < staHash.size(); i++) {
-                MeasurementAggregator ma = staHash.get(staArr[i]);
-                double min = Math.round(Double.valueOf(ma.min)) / 10.0;
-                double avg = Math.round(Double.valueOf(ma.sum) / Double.valueOf(ma.count)) / 10.0;
-                double max = Math.round(Double.valueOf(ma.max)) / 10.0;
-                out += staArr[i] + "=" + min + "/" + avg + "/" + max;
-                if (i != (staHash.size() - 1)) {
-                    out += ", ";
+            int mbs = mbuf.capacity();
+            int total = 0;
+            for (int i = 0; i < mbs; i++) {
+                byte cur = mbuf.get(i);
+                if (cur == 59) { // ascii ;
+                    total++;
                 }
             }
-            out += "}\n";
-            System.out.print(out);
+            System.out.print(total);
 
-            filBuf.close();
+            mraf.getChannel().close();
+            mraf.close();
+
+            /*
+             * BufferedReader filBuf = new BufferedReader(new FileReader(FILE));
+             * String line = filBuf.readLine();
+             * 
+             * HashMap<String, MeasurementAggregator> staHash = new HashMap<String, MeasurementAggregator>();
+             * 
+             * while (line != null) {
+             * String[] linSpl = line.split(";", 2); // station, measurement
+             * String station = linSpl[0];
+             * 
+             * MeasurementAggregator ma = staHash.get(station);
+             * if (ma == null) {
+             * ma = new MeasurementAggregator();
+             * staHash.put(station, ma);
+             * }
+             * 
+             * short tempa = Short.parseShort(linSpl[1].replace(".", "")); // x10
+             * if (tempa < ma.min) {
+             * ma.min = tempa;
+             * }
+             * if (tempa > ma.max) {
+             * ma.max = tempa;
+             * }
+             * ma.sum += tempa;
+             * ma.count++;
+             * 
+             * line = filBuf.readLine();
+             * }
+             * 
+             * String[] staArr = new String[staHash.size()];
+             * int j = 0;
+             * for (String i : staHash.keySet()) {
+             * staArr[j] = i;
+             * j++;
+             * }
+             * Arrays.sort(staArr);
+             * 
+             * String out = "{";
+             * for (int i = 0; i < staHash.size(); i++) {
+             * MeasurementAggregator ma = staHash.get(staArr[i]);
+             * double min = Math.round(Double.valueOf(ma.min)) / 10.0;
+             * double avg = Math.round(Double.valueOf(ma.sum) / Double.valueOf(ma.count)) / 10.0;
+             * double max = Math.round(Double.valueOf(ma.max)) / 10.0;
+             * out += staArr[i] + "=" + min + "/" + avg + "/" + max;
+             * if (i != (staHash.size() - 1)) {
+             * out += ", ";
+             * }
+             * }
+             * out += "}\n";
+             * System.out.print(out);
+             * 
+             * filBuf.close();
+             */
         }
         catch (IOException ex) {
             System.exit(1);
