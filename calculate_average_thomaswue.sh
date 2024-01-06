c67abfcd469cbc00f89f5850cbf64a5407e21549
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2024-01-06 01:34:11 +0100

    Minor corrections to the run script.

--- calculate_average_thomaswue.sh
+++ calculate_average_thomaswue.sh
@@ -15,15 +15,14 @@
 #  limitations under the License.
 #
 
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk use java 21.0.1-graal 1>&2
-NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -march=native --enable-preview"
-JAVA_OPTS="--enable-preview"
 
 if [ -f ./image_calculateaverage_thomaswue ]; then
     echo "Picking up existing native image, delete the file to select JVM mode." 1>&2
     time ./image_calculateaverage_thomaswue
 else
+    source "$HOME/.sdkman/bin/sdkman-init.sh"
+    sdk use java 21.0.1-graal 1>&2
+    JAVA_OPTS="--enable-preview"
     echo "Chosing to run the app in JVM mode as no native image was found, use additional_build_step_thomaswue.sh to generate." 1>&2
     time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_thomaswue
 fi
