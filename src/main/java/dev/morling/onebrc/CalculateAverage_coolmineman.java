d9000f476e3ef2704f27043dc47994b4c91e1771
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2024-01-06 10:23:00 -0600

    start

---
+++ src/main/java/dev/morling/onebrc/CalculateAverage_coolmineman.java
@@ -0,0 +1,72 @@
+/*
+ *  Copyright 2023 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package dev.morling.onebrc;
+
+import static java.util.stream.Collectors.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class CalculateAverage_coolmineman {
+
+    private static final String FILE = "./measurements.txt";
+
+    private static record Measurement(String station, double value) {
+        private Measurement(String[] parts) {
+            this(parts[0], Double.parseDouble(parts[1]));
+        }
+    }
+
+    private static class MeasurementAggregator {
+        private double min = Double.POSITIVE_INFINITY;
+        private double max = Double.NEGATIVE_INFINITY;
+        private double sum;
+        private long count;
+
+        void add(Measurement m) {
+            min = Math.min(min, m.value);
+            max = Math.max(max, m.value);
+            sum += m.value;
+            count++;
+        }
+
+        public String toString() {
+            return round(min) + "/" + round(sum / count) + "/" + round(max);
+        }
+
+        private double round(double value) {
+            return Math.round(value * 10.0) / 10.0;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Map<String, MeasurementAggregator> measurements = new TreeMap<>();
+
+        try (BufferedReader r = Files.newBufferedReader(Paths.get(FILE))) {
+            String line;
+            while ((line = r.readLine()) != null) {
+                var m = new Measurement(line.split(";"));
+                measurements.computeIfAbsent(m.station, k -> new MeasurementAggregator()).add(m);
+            }
+        }
+
+        System.out.println(measurements);
+    }
+}
