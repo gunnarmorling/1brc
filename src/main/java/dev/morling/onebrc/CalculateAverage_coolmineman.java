30d366f926dde4cf40e2d39bc7a7b7043f75a552
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2024-01-06 14:05:52 -0600

    finally faster than baseline :)

--- src/main/java/dev/morling/onebrc/CalculateAverage_coolmineman.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_coolmineman.java
@@ -33,6 +33,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.Future;
@@ -74,90 +76,123 @@ public class CalculateAverage_coolmineman {
         }
     }
 
-    private static class TrieNode {
-        TrieNode[] next;
-        MeasurementAggregator leaf;
+    static HashMap<BytesKey, MeasurementAggregator> back2basics = new HashMap<>();
 
-        TrieNode get(int c) {
-            if (next == null)
-                next = new TrieNode[256];
-            var n = next[c];
-            if (n == null) {
-                n = next[c] = new TrieNode();
+    static class BytesKey {
+        byte[] value;
+
+        BytesKey(byte[] value) {
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return new String(value, StandardCharsets.UTF_8);
+        }
+
+        @Override
+        public int hashCode() {
+            return Arrays.hashCode(value);
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof BytesKey bk) {
+                return Arrays.equals(value, bk.value);
             }
-            return n;
+            return false;
         }
 
-        MeasurementAggregator leaf() {
-            if (leaf == null)
-                leaf = new MeasurementAggregator();
-            return leaf;
-        }
-
-        void write(OutputStream os) throws IOException {
-            os.write('{' & 0xFF);
-            write(os, new ByteArrayOutputStreamEx(), true);
-            os.write('}' & 0xFF);
-        }
-
-        boolean write(OutputStream os, ByteArrayOutputStreamEx namestack, boolean first) throws IOException {
-            if (leaf != null) {
-                if (!first) {
-                    os.write(',' & 0xFF);
-                    os.write(' ' & 0xFF);
-                }
-                os.write(namestack.buf(), 0, namestack.size());
-                os.write('=');
-                os.write(leaf.toString().getBytes(StandardCharsets.UTF_8));
-                first = false;
-            }
-            if (next != null) {
-                for (int i = 0; i < 256; i++) {
-                    var n = next[i];
-                    if (n != null) {
-                        namestack.write(i);
-                        first = n.write(os, namestack, first);
-                        namestack.shrink(1);
-                    }
-                }
-            }
-            return first;
-        }
     }
 
-    static TrieNode measurements = new TrieNode();
-    static ByteArrayOutputStreamEx os = new ByteArrayOutputStreamEx();
-    static TrieNode node = measurements;
-    static boolean parsingDouble = false;
+    static void parse(ByteBuffer a, int as, ByteBuffer b, int bs, boolean isFirst) {
+        var aa = a.array();
+        var ba = b.array();
+        int pos = 0;
+        int nameEnd = 0;
+        boolean parseDouble = false;
+        int doubleStart = 0;
+        int doubleEnd = 0;
+        if (!isFirst) {
+            while (aa[pos] != (byte) '\n') {
+                pos++;
+            }
+            pos++;
+        }
+        int nameStart = pos;
+        while (pos < as) {
+            if (parseDouble) {
+                if (aa[pos] == '\n') {
+                    doubleEnd = pos;
 
-    static void parse(ByteBuffer b, int size) {
-        var a = b.array();
-        for (int i = 0; i < size; i++) {
-            byte r = a[i];
-            if (parsingDouble) {
-                if (r == (byte) '\n') {
-                    node.leaf().add(Double.parseDouble(new String(os.buf(), 0, os.size(), StandardCharsets.UTF_8)));
-                    os.reset();
-                    node = measurements;
-                    parsingDouble = false;
-                }
-                else {
-                    os.write(r);
+                    BytesKey station = new BytesKey(Arrays.copyOfRange(aa, nameStart, nameEnd));
+                    double value = Double.parseDouble(new String(aa, doubleStart, doubleEnd - doubleStart, StandardCharsets.UTF_8));
+                    back2basics.computeIfAbsent(station, k -> new MeasurementAggregator()).add(value);
+
+                    parseDouble = false;
+                    nameStart = pos + 1;
+                    nameEnd = 0;
+                    doubleStart = 0;
+                    doubleEnd = 0;
                 }
             }
             else {
-                if (r == (byte) ';') {
-                    parsingDouble = true;
-                }
-                else {
-                    node = node.get(r & 0xFF);
+                if (aa[pos] == ';') {
+                    nameEnd = pos;
+                    parseDouble = true;
+                    doubleStart = pos + 1;
                 }
             }
+
+            pos++;
+        }
+        if (bs <= 0)
+            return;
+        // TODO fix boundry case
+        for (;;) {
+            if (parseDouble) {
+                if (ba[pos - as] == '\n') {
+                    doubleEnd = pos;
+
+                    BytesKey station = new BytesKey(ofRange(a, as, b, bs, nameStart, nameEnd));
+                    // System.out.println(new String(station.value, StandardCharsets.UTF_8));
+                    double value = Double.parseDouble(new String(ofRange(a, as, b, bs, doubleStart, doubleEnd), StandardCharsets.UTF_8));
+                    // System.out.println(value);
+                    back2basics.computeIfAbsent(station, k -> new MeasurementAggregator()).add(value);
+
+                    return;
+                }
+            }
+            else {
+                if (ba[pos - as] == ';') {
+                    nameEnd = pos;
+                    parseDouble = true;
+                    doubleStart = pos + 1;
+                }
+            }
+
+            pos++;
         }
     }
 
+    static byte[] ofRange(ByteBuffer a, int as, ByteBuffer b, int bs, int start, int end) {
+        var aa = a.array();
+        var ba = b.array();
+        byte[] r = new byte[end - start];
+        for (int i = 0; i < r.length; i++) {
+            int pos = start + i;
+            if (pos < as) {
+                r[i] = aa[pos];
+            }
+            else {
+                r[i] = ba[pos - as];
+            }
+        }
+        return r;
+    }
+
     public static void main(String[] args) throws Exception {
-        int pageSize = 8192;
+        int pageSize = 80000;
         long pos = 0;
         try (AsynchronousFileChannel fc = AsynchronousFileChannel.open(Paths.get(FILE), StandardOpenOption.READ)) {
             var bbs = new ByteBuffer[4];
@@ -172,22 +207,34 @@ public class CalculateAverage_coolmineman {
                 pos += pageSize;
             }
 
+            boolean first = true;
             l: for (;;) {
                 for (int i = 0; i < bbs.length; i++) {
                     int ra = futures[i].get();
                     if (ra < 0)
                         break l;
-                    parse(bbs[i], ra);
+                    int rb = futures[(i + 1) % futures.length].get();
+                    if (rb < 0)
+                        rb = 0;
+                    parse(bbs[i], ra, bbs[(i + 1) % bbs.length], rb, first);
+                    first = false;
                     bbs[i].position(0);
                     futures[i] = fc.read(bbs[i], pos);
                     pos += pageSize;
                 }
             }
-
         }
 
-        try (OutputStream os = new BufferedOutputStream(System.out)) {
-            measurements.write(os);
-        }
+        System.out.print('{');
+        boolean[] first = new boolean[]{ true };
+        back2basics.entrySet().stream().sorted((a, b) -> a.getKey().toString().compareTo(b.getKey().toString())).forEach(e -> {
+            if (!first[0]) {
+                System.out.print(',');
+                System.out.print(' ');
+            }
+            first[0] = false;
+            System.out.print(e.toString());
+        });
+        System.out.print('}');
     }
 }
