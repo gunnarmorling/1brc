b3b88515475bc71f4b11564e62ebdf24120a8088
Thomas Wuerthinger <thomas.wuerthinger@oracle.com>
2024-01-09 19:48:33 +0100

    Add number parsing code from Quan Anh Mai. Fix name length issue.

--- src/main/java/dev/morling/onebrc/CalculateAverage_thomaswue.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_thomaswue.java
@@ -32,10 +32,10 @@ import java.util.stream.IntStream;
  * Simple solution that memory maps the input file, then splits it into one segment per available core and uses
  * sun.misc.Unsafe to directly access the mapped memory. Uses a long at a time when checking for collision.
  * <p>
- * Runs in 0.80s on my Intel i9-13900K
+ * Runs in 0.76s on my Intel i9-13900K
  * Perf stats:
- * 53,344,113,388      cpu_core/cycles/
- * 66,193,198,848      cpu_atom/cycles/
+ *    48,926,118,339      cpu_core/cycles/
+ *    53,434,680,455      cpu_atom/cycles/
  */
 public class CalculateAverage_thomaswue {
     private static final String FILE = "./measurements.txt";
@@ -116,7 +116,6 @@ public class CalculateAverage_thomaswue {
     private static void parseLoop(long chunkStart, long chunkEnd, HashMap<String, Result> cities) {
         Result[] results = new Result[1 << 18];
         long scanPtr = chunkStart;
-        byte b;
         while (scanPtr < chunkEnd) {
             long nameAddress = scanPtr;
             long hash = 0;
@@ -152,26 +151,16 @@ public class CalculateAverage_thomaswue {
             int nameLength = (int) (scanPtr - nameAddress);
             scanPtr++;
 
-            // Parse number.
-            int number;
-            byte sign = UNSAFE.getByte(scanPtr++);
-            if (sign == '-') {
-                number = UNSAFE.getByte(scanPtr++) - '0';
-                if ((b = UNSAFE.getByte(scanPtr++)) != '.') {
-                    number = number * 10 + (b - '0');
-                    scanPtr++;
-                }
-                number = number * 10 + (UNSAFE.getByte(scanPtr++) - '0');
-                number = -number;
-            }
-            else {
-                number = sign - '0';
-                if ((b = UNSAFE.getByte(scanPtr++)) != '.') {
-                    number = number * 10 + (b - '0');
-                    scanPtr++;
-                }
-                number = number * 10 + (UNSAFE.getByte(scanPtr++) - '0');
-            }
+            long numberWord = UNSAFE.getLong(scanPtr);
+            // The 4th binary digit of the ascii of a digit is 1 while
+            // that of the '.' is 0. This finds the decimal separator
+            // The value can be 12, 20, 28
+            int decimalSepPos = Long.numberOfTrailingZeros(~numberWord & 0x10101000);
+            int number = convertIntoNumber(decimalSepPos, numberWord);
+
+            // Skip past new line.
+            // scanPtr++;
+            scanPtr += (decimalSepPos >>> 3) + 3;
 
             // Final calculation for index into hash table.
             int hashAsInt = (int) (hash ^ (hash >>> 32));
@@ -186,15 +175,15 @@ public class CalculateAverage_thomaswue {
                 else {
                     // Check for collision.
                     int i = 0;
-                    for (; i < nameLength - 8; i += 8) {
+                    for (; i < nameLength + 1 - 8; i += 8) {
                         if (UNSAFE.getLong(existingResult.nameAddress + i) != UNSAFE.getLong(nameAddress + i)) {
                             tableIndex = (tableIndex + 1) & (results.length - 1);
                             continue outer;
                         }
                     }
-                    if (((UNSAFE.getLong(existingResult.nameAddress + i) ^ UNSAFE.getLong(nameAddress + i)) << (64 - (nameLength - i) << 3)) == 0) {
-                        existingResult.min = (short) Math.min(existingResult.min, number);
-                        existingResult.max = (short) Math.max(existingResult.max, number);
+                    if (((UNSAFE.getLong(existingResult.nameAddress + i) ^ UNSAFE.getLong(nameAddress + i)) << (64 - (nameLength + 1 - i) << 3)) == 0) {
+                        existingResult.min = Math.min(existingResult.min, number);
+                        existingResult.max = Math.max(existingResult.max, number);
                         existingResult.sum += number;
                         existingResult.count++;
                         break;
@@ -205,10 +194,31 @@ public class CalculateAverage_thomaswue {
                     }
                 }
             }
-
-            // Skip new line.
-            scanPtr++;
         }
+    }
+
+    // Special method to convert a number in the specific format into an int value without branches created by
+    // Quan Anh Mai.
+    private static int convertIntoNumber(int decimalSepPos, long numberWord) {
+        int shift = 28 - decimalSepPos;
+        // signed is -1 if negative, 0 otherwise
+        long signed = (~numberWord << 59) >> 63;
+        long designMask = ~(signed & 0xFF);
+        // Align the number to a specific position and transform the ascii code
+        // to actual digit value in each byte
+        long digits = ((numberWord & designMask) << shift) & 0x0F000F0F00L;
+
+        // Now digits is in the form 0xUU00TTHH00 (UU: units digit, TT: tens digit, HH: hundreds digit)
+        // 0xUU00TTHH00 * (100 * 0x1000000 + 10 * 0x10000 + 1) =
+        // 0x000000UU00TTHH00 +
+        // 0x00UU00TTHH000000 * 10 +
+        // 0xUU00TTHH00000000 * 100
+        // Now TT * 100 has 2 trailing zeroes and HH * 100 + TT * 10 + UU < 0x400
+        // This results in our value lies in the bit 32 to 41 of this product
+        // That was close :)
+        long absValue = ((digits * 0x640a0001) >>> 32) & 0x3FF;
+        long value = (absValue ^ signed) - signed;
+        return (int) value;
     }
 
     private static int findDelimiter(long word) {
