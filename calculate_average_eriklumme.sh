629adb132cd3a52bc5db6ba067030027b6ffefb3
Erik Lumme <29859656+eriklumme@users.noreply.github.com>
2024-01-09 20:49:17 +0200

    Prepare for environment with variable RAM and CPU, 0:08

--- calculate_average_eriklumme.sh
+++ calculate_average_eriklumme.sh
@@ -17,5 +17,5 @@
 
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk use java 21.0.1-graal 1>&2
-JAVA_OPTS=""
+JAVA_OPTS="-Xms6g -Xmx6g"
 time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_eriklumme
