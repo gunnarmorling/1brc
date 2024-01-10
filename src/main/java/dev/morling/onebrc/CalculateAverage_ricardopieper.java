7eca42799b5735cc59c56830cf707f5eaaecebd0
Ricardo Pieper <ricardopieper@live.com>
2024-01-10 18:48:30 -0300

    Perf improvements

--- src/main/java/dev/morling/onebrc/CalculateAverage_ricardopieper.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_ricardopieper.java
@@ -1,16 +1,39 @@
 package dev.morling.onebrc;
 
 import java.io.IOException;
+import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
-import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.*;
 
-//In some places I took inspiration in Roy's submission, but this is mostly me figuring out stuff.
-//ChatGPT 4 did help me a lot figuring out the bit mask stuff, and I gave my best shot at explaining it.
-//Unfortunately with small keys I'm having some perf issues.
+/*
+ If I had to submit something without looking what others did, I am fairly certain I would
+ be under 500MB/s on my machine on the dataset created by ./create_measurement.sh.
+
+ Maybe it's cheating? But I learned stuff
+
+
+ Credits:
+  - @royvanrijn for the idea of using bit twiddling without java.incubator.vector
+  - @royvanrijn again for the parse int only and /10 in the very end
+  - @flippingbits for the idea of not calling getLong on mmaped file directly, get a big chunk into memory instead
+  - OpenAI / ChatGPT for the actual bit twiddling idea for finding a character, it couldn't explain it so I did my best
+    (Turns out @royvanrijn had the same idea)
+
+ Note:
+  - If someone needs maybe a new trick: in the hashmap, if you read a station name as a long and it
+  fits under 8 bytes, use it directly on equality comparisons. Saves a lot of looping and gives a nice
+  ~5% boost for me.
+
+
+ Todo:
+  - The final aggregation of results is taking around 1 whole second to complete. Improve it.
+
+ */
 @SuppressWarnings("unchecked")
 public class CalculateAverage_ricardopieper {
 
@@ -37,46 +60,51 @@ public class CalculateAverage_ricardopieper {
 
         @Override
         public String toString() {
-            var min = String.format("%.1f", (double)this.min / 10.0);
-            var avg = String.format("%.1f", ((double)this.sum / (double) this.count) / 10.0);
-            var max = String.format("%.1f", (double)this.max / 10.0);
+            var min = String.format("%.1f", (double) this.min / 10.0);
+            var avg = String.format("%.1f", ((double) this.sum / (double) this.count) / 10.0);
+            var max = String.format("%.1f", (double) this.max / 10.0);
 
             return STR."\{min}/\{avg}/\{max}";
         }
     }
 
+    public static record FileChunk(Path path, long start, int size) {
+    }
+
     // This function only works for big files and numChunks > 1, otherwise some stuff
     // will break lol
-    public static List<MappedByteBuffer> splitFile(FileChannel fileChannel, int numChunks) throws IOException {
-        long size = fileChannel.size();
-        long approxSizePerChunk = size / numChunks;
-        var buffer = ByteBuffer.allocate(1024); // in practice records are max 20 chars long
-        var chunks = new ArrayList<MappedByteBuffer>();
-        long curOffset = 0;
-        for (int i = 0; i < numChunks; i++) {
-            long targetOffset = curOffset + approxSizePerChunk;
-            // advance to the target offset
-            fileChannel.position(targetOffset);
-            // advance until we find a newline
-            buffer.clear();
-            fileChannel.read(buffer);
-            int extraAdvance = 0;
-            while (buffer.get(extraAdvance) != '\n') {
-                extraAdvance++;
-            }
-            // +1 because we want to include the \n in the current chunk
-            var end = Math.min(curOffset + approxSizePerChunk + extraAdvance + 1, size);
-            long sizeOfChunk = end - curOffset;
+    public static List<FileChunk> splitFile(Path path, int numChunks) throws IOException {
+        try (var fileChannel = FileChannel.open(path, StandardOpenOption.READ)) {
+            long size = fileChannel.size();
+            long approxSizePerChunk = size / numChunks;
+            var buffer = ByteBuffer.allocate(1024); // in practice records are max 20 chars long
+            var chunks = new ArrayList<FileChunk>();
+            long curOffset = 0;
+            for (int i = 0; i < numChunks; i++) {
+                long targetOffset = curOffset + approxSizePerChunk;
+                // advance to the target offset
+                fileChannel.position(targetOffset);
+                // advance until we find a newline
+                buffer.clear();
+                fileChannel.read(buffer);
+                int extraAdvance = 0;
+                while (buffer.get(extraAdvance) != '\n') {
+                    extraAdvance++;
+                }
+                // +1 because we want to include the \n in the current chunk
+                var end = Math.min(curOffset + approxSizePerChunk + extraAdvance + 1, size);
+                long sizeOfChunk = end - curOffset;
 
-            chunks.add(fileChannel.map(
-                    FileChannel.MapMode.READ_ONLY, curOffset, sizeOfChunk));
-            curOffset = end; // because we found the \n but we want to leave at the next character
-            if (curOffset >= size) {
-                break;
+                chunks.add(new FileChunk(path,
+                        curOffset, (int) sizeOfChunk));
+                curOffset = end; // because we found the \n but we want to leave at the next character
+                if (curOffset >= size) {
+                    break;
+                }
             }
+
+            return chunks;
         }
-
-        return chunks;
     }
 
     public enum State {
@@ -117,56 +145,47 @@ public class CalculateAverage_ricardopieper {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException {
-
+        var debugMode = false;
         var path = Paths.get(FILE);
 
-        // 2* num cores, looks like helps with I/O.
-        // More than this is useless.
-        int numChunks = 2 * Runtime.getRuntime().availableProcessors();
-
-        try (var fileChannel = FileChannel.open(path, StandardOpenOption.READ)) {
-            var chunks = splitFile(fileChannel, numChunks);
-            numChunks = chunks.size();
-            MeasureMap[] maps = new MeasureMap[numChunks];
-            Thread[] threads = new Thread[numChunks];
-
-            for (int i = 0; i < numChunks; i++) {
-                int index = i;
-                var chunk = chunks.get(i);
-                var t = new Thread(() -> {
-                    maps[index] = processFileChunk(chunk);
-                });
-                threads[index] = t;
-
-            }
-            for (var t : threads) {
-                t.start();
-            }
-            for (var t : threads) {
-                t.join();
-            }
-
-            // merge maps
-            var merged = maps[0].asMap();
-
-            for (int i = 1; i < maps.length; i++) {
-                var m = maps[i];
-                for (var kv : m.asMap().entrySet()) {
-                    var mergedEntry = merged.get(kv.getKey());
-                    if (mergedEntry == null) {
-                        merged.put(kv.getKey(), kv.getValue());
-                    }
-                    else {
-                        mergedEntry.min = Math.min(mergedEntry.min, kv.getValue().min);
-                        mergedEntry.max = Math.max(mergedEntry.max, kv.getValue().max);
-                        mergedEntry.count += kv.getValue().count;
-                        mergedEntry.sum += kv.getValue().sum;
-                    }
-                }
-            }
-            System.out.println(merged);
+        var averageSizePerChunk = 16 * 1024 * 1024;
+        var fileSize = Files.size(path);
+        var numChunks = fileSize / averageSizePerChunk;
+        var chunks = splitFile(path, (int) numChunks);
+        TreeMap<String, StationMeasurements> result;
+        if (debugMode) {
+            result = processFileChunk(chunks.getFirst()).asTreeMap();
+        }
+        else {
+            result = chunks.parallelStream()
+                    .map(chunk -> {
+                        try {
+                            return processFileChunk(chunk);
+                        }
+                        catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    // .peek(x -> x.printStats())
+                    .map(MeasureMap::asTreeMap)
+                    .reduce((map1, map2) -> {
+                        for (var kv : map1.entrySet()) {
+                            var mergedEntry = map2.get(kv.getKey());
+                            if (mergedEntry == null) {
+                                map2.put(kv.getKey(), kv.getValue());
+                            }
+                            else {
+                                mergedEntry.min = Math.min(mergedEntry.min, kv.getValue().min);
+                                mergedEntry.max = Math.max(mergedEntry.max, kv.getValue().max);
+                                mergedEntry.count += kv.getValue().count;
+                                mergedEntry.sum += kv.getValue().sum;
+                            }
+                        }
+                        return map2;
+                    }).get();
         }
 
+        System.out.println(result);
     }
 
     private static final int findFirstOccurrence(long bytes, long pattern) {
@@ -223,23 +242,9 @@ public class CalculateAverage_ricardopieper {
         return Long.numberOfLeadingZeros(highBitOfSeparator) / 8;
     }
 
-    public static final long readNextLong(MappedByteBuffer chunk) {
-        int remaining = chunk.remaining();
-        if (remaining >= 8) {
-            return chunk.getLong();
-        }
-        else {
-            // get all remaining bytes
-            byte[] bytes = new byte[8];
-            chunk.get(bytes, 0, remaining);
-            return ByteBuffer.wrap(bytes).getLong();
-        }
-    }
-
-    private static MeasureMap processFileChunk(MappedByteBuffer chunk) {
+    private static MeasureMap processFileChunk(FileChunk chunk) throws IOException {
         var map = new MeasureMap();
 
-        // long size = fileChannel.size();
         long newlineMask = 0x0A0A0A0A0A0A0A0AL;
         long separatorMask = 0x3B3B3B3B3B3B3B3BL;
 
@@ -249,53 +254,48 @@ public class CalculateAverage_ricardopieper {
         int nameStart = 0, nameEnd = 0;
         int tempStart = 0, tempEnd = 0;
 
-        byte[] nameBytes = new byte[1000]; // ample space for weird unicode characters
+        // this +8 helps to remove a conditional check when calling getLong when there's less than 8 bytes remaining
+        ByteBuffer byteBuffer = ByteBuffer.allocate(chunk.size + 8);
+        try (var channel = new RandomAccessFile(chunk.path.toString(), "r")) {
+            channel.seek(chunk.start);
+            channel.read(byteBuffer.array(), 0, chunk.size);
+        }
+        byteBuffer.position(0);
 
-        long vector = 0;
-        while (chunk.hasRemaining()) {
+        while (byteBuffer.hasRemaining()) {
 
-            vector = readNextLong(chunk);
+            long nameVector = byteBuffer.getLong();
 
-            int idx = findFirstOccurrence(vector, separatorMask);
+            int idx = findFirstOccurrence(nameVector, separatorMask);
 
             boolean foundSeparator = idx != 8;
             nameEnd += idx;
             if (foundSeparator) {
                 // skip the separator
                 tempStart = nameEnd + 1;
-                tempEnd = tempStart;
             }
             else {
                 continue;
             }
 
-            chunk.position(tempStart);
-            vector = readNextLong(chunk);
+            byteBuffer.position(tempStart);
+            var tempVector = byteBuffer.getLong();
 
             // looking for temperature
             // this one is guaranteed to run in one go because
             // the temperature is max 5 characters, 6 with the newline,
             // and we start at the number already.
 
-            int newlineIdx = findFirstOccurrence(vector, newlineMask);
-            tempEnd += newlineIdx;
-
-            chunk.position(nameStart);
-            chunk.get(nameBytes, 0, nameEnd - nameStart);
+            int newlineIdx = findFirstOccurrence(tempVector, newlineMask);
+            tempEnd = tempStart + newlineIdx;
 
             // var tempStr = new String(tempBytes, 0, tempEnd - tempStart);
-            var temp = longBytesToInt(vector, tempEnd - tempStart);// Float.parseFloat(tempStr);
+            var temp = longBytesToInt(tempVector, tempEnd - tempStart);// Float.parseFloat(tempStr);
 
-            var measurements = map.getOrAdd(nameBytes, nameEnd - nameStart);
+            var measurements = map.getOrAdd(byteBuffer.array(), nameVector, nameStart, nameEnd - nameStart);
 
-            if (temp < measurements.min) {
-                measurements.min = temp;
-            }
-
-            if (temp > measurements.max) {
-                measurements.max = temp;
-            }
-
+            measurements.min = Math.min(measurements.min, temp);
+            measurements.max = Math.max(measurements.max, temp);
             measurements.sum += temp;
             measurements.count += 1;
 
@@ -303,7 +303,7 @@ public class CalculateAverage_ricardopieper {
 
             nameStart = tempEnd + 1;
             nameEnd = nameStart;
-            chunk.position(nameStart);
+            byteBuffer.position(nameStart);
         }
 
         return map;
@@ -315,7 +315,11 @@ public class CalculateAverage_ricardopieper {
     // to actually copy the underlying buffer, but the profiler showed it allocated
     // way too much memory. Performance was kind of the same though...
     public static class MeasureMap {
-        public record Entry(byte[] bytes, StationMeasurements measurements) {
+
+        // For names that are up to 8 bytes long, I just use the long I read from the file
+        // and shift it around to keep only the relevant bytes.
+        // Small strings are thus compared quickly.
+        public record Entry(byte[] bytes, long nameVector, StationMeasurements measurements) {
         }
 
         ;
@@ -333,47 +337,58 @@ public class CalculateAverage_ricardopieper {
         // I tried using a vectorized hashCode using ByteVector,
         // but I think this gets vectorized anyway because the performance is
         // either better or the same.
-        public static int hashCode(byte[] array, int length) {
-            int result = 1;
-            for (int i = 0; i < length; i++) {
-                result = 31 * result + array[i];
+        public static int hashCode(byte[] array, int offset, int length) {
+            int result = 2147483647;
+
+            for (int i = offset; i < offset + Math.min(8, length); i++) {
+                result = 131 * result + array[i];
             }
             return result;
         }
 
-        public static boolean equals(byte[] a1, int a1len, byte[] a2, int a2len) {
-            if (a1len != a2len) {
+        public boolean equals(byte[] a1, int a1offset, int a1len, byte[] a2) {
+            if (a1len != a2.length) {
                 return false;
             }
-            for (int i = 0; i < a1len; i++) {
-                if (a1[i] != a2[i]) {
+            // I tried all sorts of trickery here,
+            // like comparing first and last before the loop.
+            // It's useless. Maybe slower. Because when strings are the same length
+            // it's a pretty high chance they are the same...
+
+            for (int i = 1; i < a1len - 1; i++) {
+                if (a1[a1offset + i] != a2[i]) {
                     return false;
                 }
             }
             return true;
         }
 
-        public StationMeasurements getOrAdd(byte[] bytes, int length) {
-            var hash = hashCode(bytes, length);
+        public StationMeasurements getOrAdd(byte[] bytes, long nameVector, int offset, int length) {
+            // if the name is 3 letters, only the bottom 3 bytes are valid
+            // so let's shift left by 5
+            long shift = (64 - (length * 8));
+            long actualNameVector = nameVector << shift >> shift;
+            var hash = length >= 8 ? hashCode(bytes, offset, length) : Long.hashCode(actualNameVector);
 
             // this is a modulo operation because:
-            // buckets.lenght is a power of 2 (like 01000000, only 1 bit is set)
+            // buckets.length is a power of 2 (like 01000000, only 1 bit is set)
             // -1 makes all the bits right to that 1 become 1 (like 00111111)
             // the & will select only those 1s, and due to math, it is the remainder
-            // this has also the benefit of not needing a positive hash
-            int bucketIndex = (buckets.length - 1) & hash;
+            // this has also the benefit of not requiring a positive hash
+            int bucketIndex = ((buckets.length - 1) & hash);
 
             var bucket = buckets[bucketIndex];
             for (int i = 0; i < bucket.size(); i++) {
                 var item = bucket.get(i);
-                boolean found = equals(bytes, length, item.bytes, item.bytes.length);
+
+                boolean found = length > 8 ? equals(bytes, offset, length, item.bytes) : (actualNameVector == item.nameVector);
                 if (found) {
                     return item.measurements;
                 }
             }
 
             // new item, consolidate
-            var newItemKey = Arrays.copyOfRange(bytes, 0, length);
+            var newItemKey = Arrays.copyOfRange(bytes, offset, offset + length);
             var measurements = new StationMeasurements(
                     Integer.MAX_VALUE,
                     Integer.MIN_VALUE,
@@ -381,12 +396,12 @@ public class CalculateAverage_ricardopieper {
                     0);
 
             bucket.add(new Entry(
-                    newItemKey, measurements));
+                    newItemKey, actualNameVector, measurements));
             return measurements;
         }
 
         // Convenience method to merge maps later
-        public TreeMap<String, StationMeasurements> asMap() {
+        public TreeMap<String, StationMeasurements> asTreeMap() {
             var result = new TreeMap<String, StationMeasurements>();
             for (var bucket : this.buckets) {
                 for (var item : bucket) {
@@ -397,5 +412,31 @@ public class CalculateAverage_ricardopieper {
             return result;
         }
 
+        public void printStats() {
+            int empty = 0;
+            int one = 0;
+            int two = 0;
+            int moreThan2 = 0;
+            int moreThan5 = 0;
+            for (var bucket : this.buckets) {
+                if (bucket.isEmpty()) {
+                    empty++;
+                } else if (bucket.size() > 5) {
+                    moreThan5++;
+                } else if (bucket.size() > 2) {
+                    moreThan2++;
+                } else if (bucket.size() == 2) {
+                    two++;
+                }
+                else {
+                    one++;
+                }
+            }
+
+            System.out.println(
+                    STR."Stats: Empty = \{empty}, 1 = \{one}, 2 = \{two}, >2 = \{moreThan2}, >5 = \{moreThan5} "
+            );
+
+        }
     }
 }
