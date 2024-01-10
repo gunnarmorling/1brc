02f0fcdbfca7386bf368bdfd6445f16652fcb81b
Jason Nochlin <hundredwatt@users.noreply.github.com>
2024-01-10 11:25:20 -0700

    Script to Create a New Fork

---
+++ create_fork.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+#
+#  Copyright 2023 The original authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+set -eo pipefail
+
+SOURCE_FORK="baseline"
+
+usage() {
+  echo "Usage: create_fork.sh [-s <source fork>] <fork name>"
+  echo "  -s <source fork>  The name of the fork to copy from (default: baseline)"
+  echo "  <fork name>       The name of the fork to create"
+  exit 1
+}
+
+# Parse
+while getopts ":s:" opt; do
+  case ${opt} in
+    s )
+      SOURCE_FORK=$OPTARG
+      ;;
+    \? )
+      usage
+      exit 1
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+FORK=${@:$OPTIND:1}
+if [ -z "$FORK" ]
+  then
+    usage
+    exit 1
+fi
+
+# helper function
+function substitute_in_file {
+  if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' "s/$1/$2/g" $3
+  else
+    sed -i "s/$1/$2/g" $3
+  fi
+}
+
+set -x
+
+# create new fork
+cp prepare_$SOURCE_FORK.sh prepare_$FORK.sh
+
+cp calculate_average_$SOURCE_FORK.sh calculate_average_$FORK.sh
+substitute_in_file $SOURCE_FORK $FORK calculate_average_$FORK.sh
+
+if [ $SOURCE_FORK == "baseline" ]; then
+  cp src/main/java/dev/morling/onebrc/CalculateAverage.java src/main/java/dev/morling/onebrc/CalculateAverage_$FORK.java
+  substitute_in_file CalculateAverage CalculateAverage_$FORK src/main/java/dev/morling/onebrc/CalculateAverage_$FORK.java
+else
+  cp src/main/java/dev/morling/onebrc/CalculateAverage_$SOURCE_FORK.java src/main/java/dev/morling/onebrc/CalculateAverage_$FORK.java
+  substitute_in_file $SOURCE_FORK $FORK src/main/java/dev/morling/onebrc/CalculateAverage_$FORK.java
+fi
+
