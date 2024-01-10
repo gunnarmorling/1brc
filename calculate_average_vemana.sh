40b2fa800d0aecc373e23c87f848494e59c9273f
vemana <vemana.github@gmail.com>
2024-01-11 04:35:05 +0530

    Move to prepare_*.sh format and run evaluate2.sh locally.
    Shows 7th place in leaderboard
    
    | # | Result (m:s.ms) | Implementation     | JDK | Submitter     | Notes
    |
    |---|-----------------|--------------------|-----|---------------|-----------|
    | 1 | 00:01.588 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_thomaswue.java)|
    21.0.1-graal | [Thomas Wuerthinger](https://github.com/thomaswue) |
       GraalVM native binary |
    | 2 | 00:01.866 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_merykitty.java)|
    21.0.1-open | [Quan Anh Mai](https://github.com/merykitty) |  |
    | 3 | 00:01.904 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_royvanrijn.java)|
    21.0.1-graal | [Roy van Rijn](https://github.com/royvanrijn) |  |
    |   | 00:02.398 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_ebarlas.java)|
    21.0.1-graal | [Elliot Barlas](https://github.com/ebarlas) |  |
    |   | 00:02.724 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_obourgain.java)|
    21.0.1-open | [Olivier Bourgain](https://github.com/obourgain) |  |
    |   | 00:02.771 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_algirdasrascius.java)|
    21.0.1-open | [Algirdas Ra__ius](https://github.com/algirdasrascius) |
       |
    |   | 00:02.842 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_vemana.java)|
    21.0.1-graal | [Vemana](https://github.com/vemana) |  |
    |   | 00:02.902 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_spullara.java)|
    21.0.1-graal | [Sam Pullara](https://github.com/spullara) |  |
    |   | 00:02.906 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_artsiomkorzun.java)|
    21.0.1-graal | [artsiomkorzun](https://github.com/artsiomkorzun) |  |
    |   | 00:02.970 |
    [link](https://github.com/gunnarmorling/1brc/blob/main/src/main/java/dev/morling/onebrc/CalculateAverage_isolgpus.java)|
    21.0.1-open | [Jamie Stansfield](https://github.com/isolgpus) |  |

--- calculate_average_vemana.sh
+++ calculate_average_vemana.sh
@@ -46,13 +46,4 @@ JAVA_OPTS="$JAVA_OPTS -XX:+UseParallelGC"
 #JAVA_OPTS="$JAVA_OPTS -XX:+UseEpsilonGC -Xlog:all=off"
 #JAVA_OPTS="$JAVA_OPTS -XX:+PrintGC -XX:+PrintGCDetails"
 
-
-# Set JVM and Version
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-#sdk use java 21.0.1-open 1>&2
-sdk use java 21.0.1-graal 1>&2
-#sdk use java 21.0.1-zulu 1>&2
-#sdk use java 21.0.1-graalce 1>&2
-
-
-time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_vemana "$@"
+java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_vemana "$@"
