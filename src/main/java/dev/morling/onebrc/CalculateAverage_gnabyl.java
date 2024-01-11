176317e084d6d1e00bb54eab9bd9f76f91eebc5f
Bang Nguyen <bang.nguyen@datadoghq.com>
2024-01-11 21:50:38 +0100

    formatting

--- src/main/java/dev/morling/onebrc/CalculateAverage_gnabyl.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_gnabyl.java
@@ -30,242 +30,246 @@ import java.util.concurrent.ExecutionException;
 
 public class CalculateAverage_gnabyl {
 
-	private static final String FILE = "./measurements.txt";
+    private static final String FILE = "./measurements.txt";
 
-	private static final int NB_CHUNKS = 8;
+    private static final int NB_CHUNKS = 8;
 
-	private static record Chunk(long start, int bytesCount, MappedByteBuffer mappedByteBuffer) {
-	}
+    private static record Chunk(long start, int bytesCount, MappedByteBuffer mappedByteBuffer) {
+    }
 
-	private static int reduceSizeToFitLineBreak(FileChannel channel, long startPosition, int startSize)
-			throws IOException {
-		long currentPosition = startPosition + startSize - 1;
-		int realSize = startSize;
+    private static int reduceSizeToFitLineBreak(FileChannel channel, long startPosition, int startSize)
+            throws IOException {
+        long currentPosition = startPosition + startSize - 1;
+        int realSize = startSize;
 
-		if (currentPosition >= channel.size()) {
-			currentPosition = channel.size() - 1;
-			realSize = (int) (currentPosition - startPosition);
-		}
+        if (currentPosition >= channel.size()) {
+            currentPosition = channel.size() - 1;
+            realSize = (int) (currentPosition - startPosition);
+        }
 
-		while (currentPosition >= startPosition) {
-			channel.position(currentPosition);
-			byte byteValue = channel.map(FileChannel.MapMode.READ_ONLY, currentPosition, 1).get();
-			if (byteValue == '\n') {
-				// found line break
-				break;
-			}
+        while (currentPosition >= startPosition) {
+            channel.position(currentPosition);
+            byte byteValue = channel.map(FileChannel.MapMode.READ_ONLY, currentPosition, 1).get();
+            if (byteValue == '\n') {
+                // found line break
+                break;
+            }
 
-			realSize--;
-			currentPosition--;
-		}
-		return realSize;
-	}
+            realSize--;
+            currentPosition--;
+        }
+        return realSize;
+    }
 
-	private static List<Chunk> readChunks(long nbChunks) {
-		try (RandomAccessFile file = new RandomAccessFile(FILE, "rw")) {
-			List<Chunk> res = new ArrayList<>();
-			FileChannel channel = file.getChannel();
-			long bytesCount = channel.size();
-			long bytesPerChunk = bytesCount / nbChunks;
+    private static List<Chunk> readChunks(long nbChunks) {
+        try (RandomAccessFile file = new RandomAccessFile(FILE, "rw")) {
+            List<Chunk> res = new ArrayList<>();
+            FileChannel channel = file.getChannel();
+            long bytesCount = channel.size();
+            long bytesPerChunk = bytesCount / nbChunks;
 
-			// Memory map the file in read-only mode
-			// TODO: Optimize using threads
-			long currentPosition = 0;
-			for (int i = 0; i < nbChunks; i++) {
-				int startSize = (int) bytesPerChunk;
-				int realSize = startSize;
+            // Memory map the file in read-only mode
+            // TODO: Optimize using threads
+            long currentPosition = 0;
+            for (int i = 0; i < nbChunks; i++) {
+                int startSize = (int) bytesPerChunk;
+                int realSize = startSize;
 
-				if (i == nbChunks - 1) {
-					realSize = (int) (bytesCount - currentPosition);
-					MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, currentPosition,
-							realSize);
+                if (i == nbChunks - 1) {
+                    realSize = (int) (bytesCount - currentPosition);
+                    MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, currentPosition,
+                            realSize);
 
-					res.add(new Chunk(currentPosition, realSize, mappedByteBuffer));
-					break;
-				}
+                    res.add(new Chunk(currentPosition, realSize, mappedByteBuffer));
+                    break;
+                }
 
-				// Adjust size so that it ends on a newline
-				realSize = reduceSizeToFitLineBreak(channel, currentPosition, startSize);
+                // Adjust size so that it ends on a newline
+                realSize = reduceSizeToFitLineBreak(channel, currentPosition, startSize);
 
-				MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, currentPosition,
-						realSize);
+                MappedByteBuffer mappedByteBuffer = channel.map(FileChannel.MapMode.READ_ONLY, currentPosition,
+                        realSize);
 
-				res.add(new Chunk(currentPosition, realSize, mappedByteBuffer));
-				currentPosition += realSize;
-			}
+                res.add(new Chunk(currentPosition, realSize, mappedByteBuffer));
+                currentPosition += realSize;
+            }
 
-			channel.close();
-			file.close();
+            channel.close();
+            file.close();
 
-			return res;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return List.of();
-	}
+            return res;
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return List.of();
+    }
 
-	private static class StationData {
-		private double sum, min, max;
-		private long count;
+    private static class StationData {
+        private double sum, min, max;
+        private long count;
 
-		public StationData(double value) {
-			this.count = 1;
-			this.sum = value;
-			this.min = value;
-			this.max = value;
-		}
+        public StationData(double value) {
+            this.count = 1;
+            this.sum = value;
+            this.min = value;
+            this.max = value;
+        }
 
-		public void update(double value) {
-			this.count++;
-			this.sum += value;
-			this.min = Math.min(this.min, value);
-			this.max = Math.max(this.max, value);
-		}
+        public void update(double value) {
+            this.count++;
+            this.sum += value;
+            this.min = Math.min(this.min, value);
+            this.max = Math.max(this.max, value);
+        }
 
-		public double getMean() {
-			return sum / count;
-		}
+        public double getMean() {
+            return sum / count;
+        }
 
-		public double getMin() {
-			return min;
-		}
+        public double getMin() {
+            return min;
+        }
 
-		public double getMax() {
-			return max;
-		}
+        public double getMax() {
+            return max;
+        }
 
-		public void mergeWith(StationData other) {
-			this.sum += other.sum;
-			this.count += other.count;
-			this.min = Math.min(this.min, other.min);
-			this.max = Math.max(this.max, other.max);
-		}
+        public void mergeWith(StationData other) {
+            this.sum += other.sum;
+            this.count += other.count;
+            this.min = Math.min(this.min, other.min);
+            this.max = Math.max(this.max, other.max);
+        }
 
-	}
+    }
 
-	private static class ChunkResult {
-		private Map<String, StationData> data;
+    private static class ChunkResult {
+        private Map<String, StationData> data;
 
-		public ChunkResult() {
-			data = new HashMap<>();
-		}
+        public ChunkResult() {
+            data = new HashMap<>();
+        }
 
-		public StationData getData(String name) {
-			return data.get(name);
-		}
+        public StationData getData(String name) {
+            return data.get(name);
+        }
 
-		public void addStation(String name, double value) {
-			this.data.put(name, new StationData(value));
-		}
+        public void addStation(String name, double value) {
+            this.data.put(name, new StationData(value));
+        }
 
-		private double round(double value) {
-			return Math.round(value * 10.0) / 10.0;
-		}
+        private double round(double value) {
+            return Math.round(value * 10.0) / 10.0;
+        }
 
-		public void print() {
-			var stationNames = new ArrayList<String>(this.data.keySet());
-			Collections.sort(stationNames);
-			System.out.print("{");
-			for (int i = 0; i < stationNames.size() - 1; i++) {
-				var name = stationNames.get(i);
-				var stationData = data.get(name);
-				System.out.printf("%s=%.1f/%.1f/%.1f, ", name, round(stationData.getMin()),
-						round(stationData.getMean()),
-						round(stationData.getMax()));
-			}
-			var name = stationNames.get(stationNames.size() - 1);
-			var stationData = data.get(name);
-			System.out.printf("%s=%.1f/%.1f/%.1f", name, round(stationData.getMin()),
-					round(stationData.getMean()),
-					round(stationData.getMax()));
-			System.out.println("}");
-		}
+        public void print() {
+            var stationNames = new ArrayList<String>(this.data.keySet());
+            Collections.sort(stationNames);
+            System.out.print("{");
+            for (int i = 0; i < stationNames.size() - 1; i++) {
+                var name = stationNames.get(i);
+                var stationData = data.get(name);
+                System.out.printf("%s=%.1f/%.1f/%.1f, ", name, round(stationData.getMin()),
+                        round(stationData.getMean()),
+                        round(stationData.getMax()));
+            }
+            var name = stationNames.get(stationNames.size() - 1);
+            var stationData = data.get(name);
+            System.out.printf("%s=%.1f/%.1f/%.1f", name, round(stationData.getMin()),
+                    round(stationData.getMean()),
+                    round(stationData.getMax()));
+            System.out.println("}");
+        }
 
-		public void mergeWith(ChunkResult other) {
-			for (Map.Entry<String, StationData> entry : other.data.entrySet()) {
-				String stationName = entry.getKey();
-				StationData otherStationData = entry.getValue();
-				StationData thisStationData = this.data.get(stationName);
+        public void mergeWith(ChunkResult other) {
+            for (Map.Entry<String, StationData> entry : other.data.entrySet()) {
+                String stationName = entry.getKey();
+                StationData otherStationData = entry.getValue();
+                StationData thisStationData = this.data.get(stationName);
 
-				if (thisStationData == null) {
-					this.data.put(stationName, otherStationData);
-				} else {
-					thisStationData.mergeWith(otherStationData);
-				}
-			}
-		}
-	}
+                if (thisStationData == null) {
+                    this.data.put(stationName, otherStationData);
+                }
+                else {
+                    thisStationData.mergeWith(otherStationData);
+                }
+            }
+        }
+    }
 
-	private static ChunkResult processChunk(Chunk chunk) {
-		ChunkResult result = new ChunkResult();
+    private static ChunkResult processChunk(Chunk chunk) {
+        ChunkResult result = new ChunkResult();
 
-		// Perform processing on the chunk data
-		byte[] data = new byte[chunk.bytesCount()];
-		chunk.mappedByteBuffer().get(data);
+        // Perform processing on the chunk data
+        byte[] data = new byte[chunk.bytesCount()];
+        chunk.mappedByteBuffer().get(data);
 
-		// Process each line
-		String stationName;
-		Double value;
-		int iSplit, iEol;
-		StationData stationData;
-		for (int offset = 0; offset < data.length; offset++) {
-			// Find station name
-			for (iSplit = offset; iSplit < data.length && data[iSplit] != ';'; iSplit++) {
-			}
-			stationName = new String(data, offset, iSplit - offset, StandardCharsets.UTF_8);
+        // Process each line
+        String stationName;
+        Double value;
+        int iSplit, iEol;
+        StationData stationData;
+        for (int offset = 0; offset < data.length; offset++) {
+            // Find station name
+            for (iSplit = offset; iSplit < data.length && data[iSplit] != ';'; iSplit++) {
+            }
+            stationName = new String(data, offset, iSplit - offset, StandardCharsets.UTF_8);
 
-			// Find value
-			iSplit++;
-			for (iEol = iSplit; iEol < data.length && data[iEol] != '\n'; iEol++) {
-			}
-			value = Double.parseDouble(new String(data, iSplit, iEol - iSplit, StandardCharsets.UTF_8));
+            // Find value
+            iSplit++;
+            for (iEol = iSplit; iEol < data.length && data[iEol] != '\n'; iEol++) {
+            }
+            value = Double.parseDouble(new String(data, iSplit, iEol - iSplit, StandardCharsets.UTF_8));
 
-			// Init & count
-			stationData = result.getData(stationName);
+            // Init & count
+            stationData = result.getData(stationName);
 
-			if (stationData == null) {
-				result.addStation(stationName, value);
-			} else {
-				stationData.update(value);
-			}
+            if (stationData == null) {
+                result.addStation(stationName, value);
+            }
+            else {
+                stationData.update(value);
+            }
 
-			offset = iEol;
-		}
+            offset = iEol;
+        }
 
-		return result;
-	}
+        return result;
+    }
 
-	private static ChunkResult processAllChunks(List<Chunk> chunks) {
-		// var globalRes = new ChunkResult();
-		// for (var chunk : chunks) {
-		// var chunkRes = processChunk(chunk);
-		// globalRes.mergeWith(chunkRes);
-		// }
-		// return globalRes;
+    private static ChunkResult processAllChunks(List<Chunk> chunks) {
+        // var globalRes = new ChunkResult();
+        // for (var chunk : chunks) {
+        // var chunkRes = processChunk(chunk);
+        // globalRes.mergeWith(chunkRes);
+        // }
+        // return globalRes;
 
-		List<CompletableFuture<ChunkResult>> computeTasks = new ArrayList<>();
+        List<CompletableFuture<ChunkResult>> computeTasks = new ArrayList<>();
 
-		for (Chunk chunk : chunks) {
-			computeTasks.add(CompletableFuture.supplyAsync(() -> processChunk(chunk)));
-		}
+        for (Chunk chunk : chunks) {
+            computeTasks.add(CompletableFuture.supplyAsync(() -> processChunk(chunk)));
+        }
 
-		ChunkResult globalRes = new ChunkResult();
+        ChunkResult globalRes = new ChunkResult();
 
-		for (CompletableFuture<ChunkResult> completedTask : computeTasks) {
-			try {
-				ChunkResult chunkRes = completedTask.get();
-				globalRes.mergeWith(chunkRes);
-			} catch (InterruptedException | ExecutionException e) {
-				e.printStackTrace(); // Handle exceptions if needed
-			}
-		}
+        for (CompletableFuture<ChunkResult> completedTask : computeTasks) {
+            try {
+                ChunkResult chunkRes = completedTask.get();
+                globalRes.mergeWith(chunkRes);
+            }
+            catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace(); // Handle exceptions if needed
+            }
+        }
 
-		return globalRes;
-	}
+        return globalRes;
+    }
 
-	public static void main(String[] args) throws IOException {
-		var chunks = readChunks(NB_CHUNKS);
-		var result = processAllChunks(chunks);
-		result.print();
-	}
+    public static void main(String[] args) throws IOException {
+        var chunks = readChunks(NB_CHUNKS);
+        var result = processAllChunks(chunks);
+        result.print();
+    }
 }
