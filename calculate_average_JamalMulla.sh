3cf1ce14af323ee8db6885a182d183b1d3e0d932
Jamal Mulla <j.mulla@mwam.com>
2024-01-11 09:38:33 +0000

    Script reorg

--- calculate_average_JamalMulla.sh
+++ calculate_average_JamalMulla.sh
@@ -15,7 +15,5 @@
 #  limitations under the License.
 #
 
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk use java 21.0.1-graal 1>&2
 JAVA_OPTS="--enable-preview -XX:+UnlockExperimentalVMOptions -XX:+TrustFinalNonStaticFields -XX:+UseTransparentHugePages"
 time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_JamalMulla
