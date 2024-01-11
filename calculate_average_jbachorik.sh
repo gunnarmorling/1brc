71d9b4b8530a816026334aa465c1b15391da5fc3
Jaroslav Bachorik <j.bachorik@gmail.com>
2024-01-11 18:08:18 +0100

    Launcher tweaks

--- calculate_average_jbachorik.sh
+++ calculate_average_jbachorik.sh
@@ -16,5 +16,6 @@
 #
 
 source ~/.sdkman/bin/sdkman-init.sh > /dev/null
-sdk use java 21.0.1-graal > /dev/null
+sdk use java 21.0.1-tem > /dev/null
+JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC  -Xmx512m -XX:+UseInlineCaches -XX:CICompilerCount=4 -Xmx512m -XX:+UseInlineCaches -XX:MaxInlineSize=512 -XX:FreqInlineSize=800 -XX:InlineSmallCode=190 -XX:CompileThreshold=2"
 time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_jbachorik $@
