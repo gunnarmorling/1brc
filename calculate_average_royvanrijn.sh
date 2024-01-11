8c248714061535a819181ca137876fa1435a7e7d
Roy van Rijn <roy.van.rijn@gmail.com>
2024-01-11 11:12:05 +0100

    Fixing the off-by-one error and updating to native, redone layout of code. (#307)

--- calculate_average_royvanrijn.sh
+++ calculate_average_royvanrijn.sh
@@ -15,5 +15,16 @@
 #  limitations under the License.
 #
 
-JAVA_OPTS="--enable-preview"
-java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_royvanrijn
+if [ -f target/CalculateAverage_royvanrijn_image ]; then
+    echo "Picking up existing native image 'target/CalculateAverage_royvanrijn_image', delete the file to select JVM mode." 1>&2
+    target/CalculateAverage_royvanrijn_image
+else
+    JAVA_OPTS="--enable-preview -XX:+UnlockExperimentalVMOptions -XX:+TrustFinalNonStaticFields -dsa -XX:+UseNUMA"
+    if [[ ! "$(uname -s)" = "Darwin" ]]; then
+        # On OS/X, my machine, this errors:
+        JAVA_OPTS="$JAVA_OPTS -XX:+UseTransparentHugePages"
+    fi
+    echo "Choosing to run the app in JVM mode as no native image was found, use additional_build_step_royvanrijn.sh to generate." 1>&2
+    java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_royvanrijn
+fi
+
