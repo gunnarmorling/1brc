feee2420b872aaa409702dc15a4a3c3be477a97c
Gunnar Morling <gunnar.morling@googlemail.com>
2024-01-11 22:15:07 +0100

    Leaderboard update

--- calculate_average_jbachorik.sh
+++ calculate_average_jbachorik.sh
@@ -16,4 +16,4 @@
 #
 
 JAVA_OPTS="-XX:+UseInlineCaches -XX:CICompilerCount=4 -Xmx512m -XX:+UseInlineCaches -XX:MaxInlineSize=512 -XX:FreqInlineSize=800 -XX:InlineSmallCode=190 -XX:CompileThreshold=2"
-time java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_jbachorik $@
+java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_jbachorik $@
