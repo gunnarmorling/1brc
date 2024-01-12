c2ee852ecbcc09f88573d3127dfb8c910ff7d942
Bang NGUYEN <lelybang.nguyen@gmail.com>
2024-01-12 16:27:53 +0100

    Update class name in calculate_average_baseline.sh

--- calculate_average_baseline.sh
+++ calculate_average_baseline.sh
@@ -16,4 +16,4 @@
 #
 
 JAVA_OPTS=""
-java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage
+java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_baseline
