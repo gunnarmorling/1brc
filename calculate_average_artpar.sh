f37b304fc3006933dd1245218c41404c336df280
Parth Mudgal <artpar@gmail.com>
2024-01-12 14:08:09 +0530

    inline hash calculation and number parsing (#200)
    no number parsing with precalculated map
    verify tests
    better loop with direct hash to measurement mapping
    accept formatting changes
    Use unsafe

--- calculate_average_artpar.sh
+++ calculate_average_artpar.sh
@@ -16,5 +16,5 @@
 #
 
 
-JAVA_OPTS="--add-modules=jdk.incubator.vector"
+JAVA_OPTS="--enable-preview"
 java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_artpar
