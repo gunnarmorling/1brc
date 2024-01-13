479a843f676e1bc1f1e94f5466e6d91100b7f843
Stefan Sprenger <stefan@datacater.io>
2024-01-13 09:49:10 +0100

    feat(flippingbits): Use custom hash map

--- src/main/java/dev/morling/onebrc/CalculateAverage_flippingbits.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_flippingbits.java
@@ -40,8 +40,12 @@ public class CalculateAverage_flippingbits {
 
     private static final int MAX_STATION_NAME_LENGTH = 100;
 
+    private static final int NUM_STATIONS = 10_000;
+
+    private static final int FASTER_HASH_MAP_CAPACITY = 200_000;
+
     public static void main(String[] args) throws IOException {
-        var result = Arrays.asList(getSegments()).stream()
+        var result = Arrays.asList(getSegments()).parallelStream()
                 .map(segment -> {
                     try {
                         return processSegment(segment[0], segment[1]);
@@ -50,22 +54,15 @@ public class CalculateAverage_flippingbits {
                         throw new RuntimeException(e);
                     }
                 })
-                .parallel()
-                .reduce((firstMap, secondMap) -> {
-                    for (var entry : secondMap.entrySet()) {
-                        PartitionAggregate firstAggregate = firstMap.get(entry.getKey());
-                        if (firstAggregate == null) {
-                            firstMap.put(entry.getKey(), entry.getValue());
-                        }
-                        else {
-                            firstAggregate.mergeWith(entry.getValue());
-                        }
-                    }
-                    return firstMap;
-                })
-                .map(TreeMap::new).get();
+                .reduce(FasterHashMap::mergeWith)
+                .get();
 
-        System.out.println(result);
+        var sortedMap = new TreeMap<String, Station>();
+        for (Station station : result.getValues()) {
+            sortedMap.put(station.getParsedName(), station);
+        }
+
+        System.out.println(sortedMap);
     }
 
     private static long[][] getSegments() throws IOException {
@@ -100,27 +97,25 @@ public class CalculateAverage_flippingbits {
         }
     }
 
-    private static Map<String, PartitionAggregate> processSegment(long startOfSegment, long endOfSegment)
+    private static FasterHashMap processSegment(long startOfSegment, long endOfSegment)
             throws IOException {
-        Map<String, PartitionAggregate> stationAggregates = new HashMap<>(50_000);
+        var fasterHashMap = new FasterHashMap();
         var byteChunk = new byte[(int) (endOfSegment - startOfSegment)];
-        var stationBuffer = new byte[MAX_STATION_NAME_LENGTH];
+        var nameBuffer = new byte[MAX_STATION_NAME_LENGTH];
         try (var file = new RandomAccessFile(FILE, "r")) {
             file.seek(startOfSegment);
             file.read(byteChunk);
             var i = 0;
             while (i < byteChunk.length) {
                 // Station name has at least one byte
-                stationBuffer[0] = byteChunk[i];
-                i++;
+                nameBuffer[0] = byteChunk[i++];
                 // Read station name
-                var j = 1;
+                var nameLength = 1;
+                var nameHash = 0;
                 while (byteChunk[i] != ';') {
-                    stationBuffer[j] = byteChunk[i];
-                    j++;
-                    i++;
+                    nameHash = nameHash * 31 + byteChunk[i];
+                    nameBuffer[nameLength++] = byteChunk[i++];
                 }
-                var station = new String(stationBuffer, 0, j, StandardCharsets.UTF_8);
                 i++;
 
                 // Read measurement
@@ -143,33 +138,49 @@ public class CalculateAverage_flippingbits {
                 }
 
                 // Update aggregate
-                var aggregate = stationAggregates.computeIfAbsent(station, x -> new PartitionAggregate());
-                aggregate.addMeasurementAndComputeAggregate((short) measurement);
+                fasterHashMap.addEntry(nameHash, nameBuffer, nameLength, (short) measurement);
                 i += 3;
             }
-            stationAggregates.values().forEach(PartitionAggregate::aggregateRemainingMeasurements);
         }
 
-        return stationAggregates;
+        for (Station station : fasterHashMap.getValues()) {
+            station.aggregateRemainingMeasurements();
+        }
+
+        return fasterHashMap;
     }
 
-    private static class PartitionAggregate {
-        final short[] doubleLane = new short[SIMD_LANE_LENGTH * 2];
+    private static class Station {
+        final short[] measurements = new short[SIMD_LANE_LENGTH * 2];
         // Assume that we do not have more than Integer.MAX_VALUE measurements for the same station per partition
-        int count = 0;
+        int count = 1;
         long sum = 0;
         short min = Short.MAX_VALUE;
         short max = Short.MIN_VALUE;
+        final byte[] name;
+        final int nameLength;
+        final int nameHash;
+
+        public Station(int nameLength, byte[] name, int nameHash, short measurement) {
+            this.nameLength = nameLength;
+            this.name = name.clone();
+            this.nameHash = nameHash;
+            measurements[0] = measurement;
+        }
+
+        public String getParsedName() {
+            return new String(name, 0, nameLength, StandardCharsets.UTF_8);
+        }
 
         public void addMeasurementAndComputeAggregate(short measurement) {
             // Add measurement to buffer, which is later processed by SIMD instructions
-            doubleLane[count % doubleLane.length] = measurement;
+            measurements[count % measurements.length] = measurement;
             count++;
 
             // Once lane is full, use SIMD instructions to calculate aggregates
-            if (count % doubleLane.length == 0) {
-                var firstVector = ShortVector.fromArray(ShortVector.SPECIES_MAX, doubleLane, 0);
-                var secondVector = ShortVector.fromArray(ShortVector.SPECIES_MAX, doubleLane, SIMD_LANE_LENGTH);
+            if (count % measurements.length == 0) {
+                var firstVector = ShortVector.fromArray(ShortVector.SPECIES_MAX, measurements, 0);
+                var secondVector = ShortVector.fromArray(ShortVector.SPECIES_MAX, measurements, SIMD_LANE_LENGTH);
 
                 var simdMin = firstVector.min(secondVector).reduceLanes(VectorOperators.MIN);
                 min = (short) Math.min(min, simdMin);
@@ -182,15 +193,15 @@ public class CalculateAverage_flippingbits {
         }
 
         public void aggregateRemainingMeasurements() {
-            for (var i = 0; i < count % doubleLane.length; i++) {
-                var measurement = doubleLane[i];
+            for (var i = 0; i < count % measurements.length; i++) {
+                var measurement = measurements[i];
                 min = (short) Math.min(min, measurement);
                 max = (short) Math.max(max, measurement);
                 sum += measurement;
             }
         }
 
-        public void mergeWith(PartitionAggregate otherAggregate) {
+        public void mergeWith(Station otherAggregate) {
             min = (short) Math.min(min, otherAggregate.min);
             max = (short) Math.max(max, otherAggregate.max);
             count = count + otherAggregate.count;
@@ -204,6 +215,78 @@ public class CalculateAverage_flippingbits {
                     (min / 10.0),
                     ((sum / 10.0) / count),
                     (max / 10.0));
+        }
+    }
+
+    /**
+     * Use two arrays for implementing the hash map:
+     * - The array `entries` holds the map values, in our case instances of the class Station.
+     * - The array `offsets` maps hashes of the keys to indexes in the `entries` array.
+     *
+     * We create `offsets` with a much larger capacity than `entries`, so we minimize collisions.
+     */
+    private static class FasterHashMap {
+        // Using 16-bit integers (shorts) for offsets supports up to 2^15 (=32,767) entries
+        // If you need to store more entries, consider replacing short with int
+        short[] offsets = new short[FASTER_HASH_MAP_CAPACITY];
+        Station[] entries = new Station[NUM_STATIONS + 1];
+        int slotsInUse = 0;
+
+        private boolean arraysAreEqual(byte[] a, byte[] b, int lengthA, int lengthB) {
+            if (lengthA != lengthB) {
+                return false;
+            }
+            for (var i = 0; i < lengthA; i++) {
+                if (a[i] != b[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private int getOffsetIdx(int nameHash, byte[] name, int nameLength) {
+            var offsetIdx = nameHash & (offsets.length - 1);
+            var offset = offsets[offsetIdx];
+
+            while (offset != 0 && !arraysAreEqual(entries[offset].name, name, entries[offset].nameLength, nameLength)) {
+                offsetIdx = (offsetIdx + 1) % offsets.length;
+                offset = offsets[offsetIdx];
+            }
+
+            return offsetIdx;
+        }
+
+        public void addEntry(int nameHash, byte[] name, int nameLength, short measurement) {
+            var offsetIdx = getOffsetIdx(nameHash, name, nameLength);
+            var offset = offsets[offsetIdx];
+            if (offset == 0) {
+                slotsInUse++;
+                entries[slotsInUse] = new Station(nameLength, name, nameHash, measurement);
+                offsets[offsetIdx] = (short) slotsInUse;
+            }
+            else {
+                entries[offset].addMeasurementAndComputeAggregate(measurement);
+            }
+        }
+
+        public FasterHashMap mergeWith(FasterHashMap otherMap) {
+            for (Station station : otherMap.getValues()) {
+                var offsetIdx = getOffsetIdx(station.nameHash, station.name, station.nameLength);
+                var offset = offsets[offsetIdx];
+                if (offset == 0) {
+                    slotsInUse++;
+                    entries[slotsInUse] = station;
+                    offsets[offsetIdx] = (short) slotsInUse;
+                }
+                else {
+                    entries[offset].mergeWith(station);
+                }
+            }
+            return this;
+        }
+
+        public List<Station> getValues() {
+            return Arrays.asList(entries).subList(1, slotsInUse + 1);
         }
     }
 }
