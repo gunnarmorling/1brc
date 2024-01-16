ae9bbb13571ef2917d46f821fa8be47ff9b6b8a1
Antonio Muñoz <antoniogmc@gmail.com>
2024-01-16 23:02:19 +0100

    remove any parallelization

--- src/main/java/dev/morling/onebrc/CalculateAverage_tonivade.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_tonivade.java
@@ -20,12 +20,9 @@ import static java.util.stream.Collectors.toMap;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BinaryOperator;
 
 public class CalculateAverage_tonivade {
@@ -45,89 +42,43 @@ public class CalculateAverage_tonivade {
     }
 
     public static void main(String[] args) throws IOException {
-        Map<String, Processor> map = new ConcurrentHashMap<>();
+        Map<String, Station> map = new HashMap<>();
 
         Files.lines(Paths.get(FILE))
-                .parallel()
                 .forEach(line -> {
                     var index = line.indexOf(';');
-                    var station = line.substring(0, index);
-                    var value = line.substring(index + 1);
-                    map.computeIfAbsent(station, Processor::new).put(value);
+                    var name = line.substring(0, index);
+                    var value = Double.parseDouble(line.substring(index + 1));
+                    map.computeIfAbsent(name, Station::new).add(value);
                 });
 
         Map<String, ResultRow> measurements = map.values().stream()
-                .collect(toMap(Processor::getStation, Processor::finish, throwingMerger(), TreeMap::new));
+                .collect(toMap(Station::getName, Station::finish, throwingMerger(), TreeMap::new));
 
         System.out.println(measurements);
     }
 
     private static BinaryOperator<ResultRow> throwingMerger() {
-      return (a, b) -> {
-          throw new IllegalStateException("Duplicated key exception");
-      };
+        return (a, b) -> {
+            throw new IllegalStateException("Duplicated key exception");
+        };
     }
 
-    static final class Processor implements Runnable {
+    static final class Station {
 
-        private final String station;
-        private final Thread thread;
+        private final String name;
 
         private double min = Double.POSITIVE_INFINITY;
         private double max = Double.NEGATIVE_INFINITY;
         private double sum;
         private long count;
 
-        private final BlockingQueue<String> queue = new ArrayBlockingQueue<>(1000);
-        private final AtomicBoolean stop = new AtomicBoolean();
-        private final AtomicBoolean finished = new AtomicBoolean();
-
-        Processor(String station) {
-            this.station = station;
-            this.thread = Thread.ofVirtual().name(station).start(this);
+        Station(String name) {
+            this.name = name;
         }
 
-        @Override
-        public void run() {
-            while (!stop.get()) {
-                try {
-                    add(Double.parseDouble(queue.take()));
-                }
-                catch (InterruptedException e) {
-                    // ignore
-                }
-            }
-            // drain queue
-            queue.stream().map(Double::parseDouble).forEach(this::add);
-            finished.set(true);
-        }
-
-        String getStation() {
-            return station;
-        }
-
-        void put(String measurement) {
-            try {
-                queue.put(measurement);
-            }
-            catch (InterruptedException e) {
-                // ignore
-            }
-        }
-
-        ResultRow finish() {
-            stop.set(true);
-            thread.interrupt();
-            while (!finished.get()) {
-                try {
-                    Thread.sleep(10);
-                }
-                catch (InterruptedException e) {
-                    // ignore
-                }
-            }
-            return new ResultRow(
-                    this.min, (Math.round(this.sum * 10.0) / 10.0) / this.count, this.max);
+        String getName() {
+            return name;
         }
 
         void add(double value) {
@@ -135,6 +86,11 @@ public class CalculateAverage_tonivade {
             max = Math.max(max, value);
             sum += value;
             count++;
+        }
+
+        ResultRow finish() {
+            return new ResultRow(
+                    this.min, (Math.round(this.sum * 10.0) / 10.0) / this.count, this.max);
         }
     }
 }
