5775e1e570bc3e9c1bb17e68f2b024d1cd2f838a
Stanley Lim <slim679975@gmail.com>
2024-01-16 01:51:44 -0800

    :sparkles: feat: Changed to single linear pass, use indexOf instead of string tokenizer, other changes

--- src/main/java/dev/morling/onebrc/CalculateAverage_spiderpig86.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_spiderpig86.java
@@ -18,32 +18,43 @@ package dev.morling.onebrc;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 /**
  * Changelog:
  *      - 1/15/24 - initial minor changes with syntax. 100k lines - 209ms
- *      - 1/15/24 - new algorithm without streams, new splitting method, use of BufferedReader. 100k lines - 154ms,
- *      1m lines - 578ms
+ *      - 1/15/24 - new algorithm without streams, new splitting method, use of BufferedReader. https://stackoverflow.com/questions/11001330/java-split-string-performances
+ *          100k lines - 154ms,
+ *          1m lines - 578ms
+ *      - 1/16/24 - changed to single linear pass, use indexOf instead of string tokenizer, other changes.
+ *          1m lines - 544ms
  */
 public class CalculateAverage_spiderpig86 {
 
     private static final String FILE = "./measurements2.txt";
 
-    private record Measurement(String station, double value) {
-        private Measurement(String[] parts) {
-            this(parts[0], Double.parseDouble(parts[1]));
-        }
-    }
+    private record Measurement(String station, double value) { }
 
-    private record ResultRow(double min, double mean, double max) {
+    private static class ResultRow {
+        private double min = Double.POSITIVE_INFINITY;
+        private double max = Double.NEGATIVE_INFINITY;
+        private double total = 0.0;
+        private long count = 0;
+
+        public void processValue(double val) {
+            min = Math.min(min, val);
+            max = Math.max(max, val);
+            total += val;
+            ++count;
+        }
+
+        public double getMean() {
+            return total / count;
+        }
+
         public String toString() {
-            return round(min) + "/" + round(mean) + "/" + round(max);
+            return round(min) + "/" + round(getMean()) + "/" + round(max);
         }
 
         private double round(double value) {
@@ -56,7 +67,7 @@ public class CalculateAverage_spiderpig86 {
         long start = System.currentTimeMillis();
 
         // Read file
-        Map<String, List<Measurement>> stations = new HashMap<>();
+        Map<String, ResultRow> stations = new TreeMap<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(FILE))) {
             String line;
 
@@ -64,9 +75,9 @@ public class CalculateAverage_spiderpig86 {
             while ((line = reader.readLine()) != null) {
                 Measurement measurement = parseRow(line);
                 if (!stations.containsKey(measurement.station)) {
-                    stations.put(measurement.station, new ArrayList<>());
+                    stations.put(measurement.station, new ResultRow());
                 }
-                stations.get(measurement.station).add(measurement);
+                stations.get(measurement.station).processValue(measurement.value());
             }
         }
         catch (IOException e) {
@@ -74,38 +85,15 @@ public class CalculateAverage_spiderpig86 {
             return;
         }
 
-        // For each station, find the min, max, and mean. Station names must be in sorted order
-        Map<String, ResultRow> result = new TreeMap<>();
-        for (Map.Entry<String, List<Measurement>> entry : stations.entrySet()) {
-            result.put(entry.getKey(), getStats(entry.getValue()));
-        }
-
-        System.out.println(result);
+        System.out.println(stations);
         // TODO Remove
          System.out.println("Elapsed time ms: " + (System.currentTimeMillis() - start));
     }
 
     private static Measurement parseRow(String row) {
-        // Testing faster String splits
-        // https://stackoverflow.com/questions/11001330/java-split-string-performances
-        StringTokenizer stringTokenizer = new StringTokenizer(row, ";");
-
-        return new Measurement(new String[]{ stringTokenizer.nextToken(), stringTokenizer.nextToken() });
-    }
-
-    private static ResultRow getStats(List<Measurement> measurements) {
-        double min = Double.POSITIVE_INFINITY;
-        double max = Double.NEGATIVE_INFINITY;
-        double sum = 0.0;
-        long count = 0;
-
-        for (Measurement m : measurements) {
-            min = Math.min(min, m.value);
-            max = Math.max(max, m.value);
-            ++count;
-            sum += m.value;
-        }
-
-        return new ResultRow(min, (Math.round(sum * 10.0) / 10.0) / count, max);
+        // Substring is faster than split by a long shot
+        // https://stackoverflow.com/questions/13997361/string-substring-vs-string-split#:~:text=When%20you%20run%20this%20multiple,would%20be%20even%20more%20drastic.
+        int delimiterIndex = row.indexOf(';');
+        return new Measurement(row.substring(0, delimiterIndex), Double.parseDouble(row.substring(delimiterIndex + 1)));
     }
 }
