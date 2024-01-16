d197a527716b1a99e3f2de238daacfd11bba65c9
Matteo Vaccari <mvaccari@thoughtworks.com>
2024-01-16 15:21:13 +0100

    Now using a state machine for parsing!

--- calculate_average.go
+++ calculate_average.go
@@ -33,7 +33,7 @@ func splitSemicolonOrNewline(data []byte, atEOF bool) (advance int, token []byte
 func main() {
 	name := dataFileName()
 	data := readData(name)
-	cities := parseData(data)
+	cities := parseData1(data)
 	printCities(cities)
 }
 
@@ -58,6 +58,73 @@ func readData(name string) []byte {
 	return data
 }
 
+/*
+State machine
+
+start , (not ;) -> cityState 		, init cityState name
+cityState  , (not ;) -> cityState			, accumulate cityState name
+cityState  , ; 		-> semicolon	, terminate cityState name
+semicolon , [0-9] 	-> temp			, init temperature
+temp , [0-9] 	-> temp			, accumulate temperature
+temp , '.'		-> temp			, do nothing
+temp , '\n'		-> start		, save measurement
+*/
+
+type state struct {
+	name string
+}
+
+var (
+	cityState   = state{"cityState"}
+	semicolon   = state{"semicolon"}
+	temperature = state{"temperature"}
+)
+
+func parseData1(data []byte) map[string]cityData {
+	cities := make(map[string]cityData)
+	state := cityState
+	var cityStartOffset, cityEndOffset int
+	var temp, sign int
+
+	cityStartOffset = 0
+	for i := 0; i < len(data); i++ {
+		currentChar := data[i]
+		//fmt.Printf("%02d %c\n", i, currentChar)
+		if state == cityState && currentChar == ';' {
+			state = semicolon
+			cityEndOffset = i
+		} else if state == cityState && (currentChar&0x80 == 0) {
+			// do nothing
+		} else if state == cityState && (currentChar&0xE0 == 0xC0) {
+			i++ // 2-byte utf8 char
+		} else if state == cityState && (currentChar&0xF0 == 0xE0) {
+			i += 2 // 3-byte utf8 char
+		} else if state == cityState && (currentChar&0xF8 == 0xF0) {
+			i += 3 // 4-byte utf8 char
+		} else if state == semicolon && currentChar == '-' {
+			state = temperature
+			temp = 0
+			sign = -1
+		} else if state == semicolon && currentChar >= '0' && currentChar <= '9' {
+			state = temperature
+			temp = int(currentChar - '0')
+			sign = 1
+		} else if state == temperature && currentChar >= '0' && currentChar <= '9' {
+			temp = temp*10 + int(currentChar-'0')
+		} else if state == temperature && currentChar == '.' {
+			// do nothing
+		} else if state == temperature && currentChar == '\n' {
+			city := string(data[cityStartOffset:cityEndOffset])
+			accumulate(cities, city, temp*sign)
+			state = cityState
+			cityStartOffset = i + 1
+		} else {
+			panic(fmt.Sprintf("Unexpected: %s, %c", state, currentChar))
+		}
+	}
+	return cities
+}
+
 func parseData(data []byte) map[string]cityData {
 	cities := make(map[string]cityData)
 	scanner := bufio.NewScanner(bytes.NewReader(data))
@@ -73,24 +140,28 @@ func parseData(data []byte) map[string]cityData {
 		if err != nil {
 			panic(err)
 		}
-		if previous, ok := cities[city]; !ok {
-			cities[city] = cityData{tempAsInt, tempAsInt, tempAsInt, 1}
-		} else {
-			var newMin, newMax int
-			if tempAsInt < previous.min {
-				newMin = tempAsInt
-			} else {
-				newMin = previous.min
-			}
-			if tempAsInt > previous.max {
-				newMax = tempAsInt
-			} else {
-				newMax = previous.max
-			}
-			cities[city] = cityData{newMin, previous.total + tempAsInt, newMax, previous.count + 1}
-		}
+		accumulate(cities, city, tempAsInt)
 	}
 	return cities
+}
+
+func accumulate(cities map[string]cityData, city string, tempAsInt int) {
+	if previous, ok := cities[city]; !ok {
+		cities[city] = cityData{tempAsInt, tempAsInt, tempAsInt, 1}
+	} else {
+		var newMin, newMax int
+		if tempAsInt < previous.min {
+			newMin = tempAsInt
+		} else {
+			newMin = previous.min
+		}
+		if tempAsInt > previous.max {
+			newMax = tempAsInt
+		} else {
+			newMax = previous.max
+		}
+		cities[city] = cityData{newMin, previous.total + tempAsInt, newMax, previous.count + 1}
+	}
 }
 
 func printCities(cities map[string]cityData) {
