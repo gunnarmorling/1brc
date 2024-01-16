822e14029ea893443c0dca8ad07b0ffcfac7f598
Gunnar Morling <gunnar.morling@googlemail.com>
2024-01-16 22:23:22 +0100

    Update src/main/java/dev/morling/onebrc/CalculateAverage_adriacabeza.java

--- src/main/java/dev/morling/onebrc/CalculateAverage_adriacabeza.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_adriacabeza.java
@@ -28,7 +28,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
 /**
  * This class calculates average measurements from a file in a parallelized manner.
  */
