24e16f04a44e12c2f95296b5ae07142ba68ece29
Gerd Aschemann <gerd@aschemann.net>
2024-01-17 13:29:21 +0100

    Test solutions with unique station names

---
+++ reset-sdk.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk u java 21.0.1-tem
---
+++ test-unique.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -euo pipefail
+
+: "${UNIQUE:=100000}"
+
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <gh-user>" >&2
+  exit 1
+fi
+user="${1}"
+
+exe="calculate_average_${1}.sh"
+
+if [ ! -x "${exe}" ]; then
+  echo "Start script '${exe}' does not exist" >&2
+  exit 2
+fi
+
+testdata="measurements-unique-${UNIQUE}.txt"
+if [ ! -r "${testdata}" ]; then
+  ./create_measurements_unique.sh "${UNIQUE}"
+fi
+
+ln -sfn "${testdata}" "measurements.txt"
+
+mkdir -p results
+
+baseline="results/result-unique-${UNIQUE}-baseline.txt"
+if [ ! -r "${baseline}" ]; then
+  echo "Baseline result '${baseline}' does not yet exists, we create it first!" >&2
+  ./prepare_baseline.sh
+  ./calculate_average_baseline.sh > "${baseline}"
+fi
+
+prep="prepare_${user}.sh"
+test -x "${prep}" && ./${prep}
+
+./reset-sdk.sh
+
+result="results/result-unique-${UNIQUE}-${user}.txt"
+./${exe} > "${result}"
+
+if ! diff -q "${result}" "${baseline}"; then
+  ls -l "${result}" "${baseline}"
+fi
