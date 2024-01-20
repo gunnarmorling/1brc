ea8831616748cd413ea60945d06b0cd3e0b7ffc5
Elliot Barlas <elliotbarlas@gmail.com>
2024-01-19 23:04:02 -0800

    Use Arena MemorySegments rather than ByteBuffers.

--- calculate_average_ebarlas.sh
+++ calculate_average_ebarlas.sh
@@ -15,5 +15,5 @@
 #  limitations under the License.
 #
 
-JAVA_OPTS=""
+JAVA_OPTS="--enable-preview"
 java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_ebarlas
