cd18af5bf309993fd81e8c21a466931dd9f1d985
Michal Jonko <michal.jonko@neotechnology.com>
2024-01-21 21:20:16 +0100

    extended hash calculations to prevent hash collisions for city names

--- src/main/java/dev/morling/onebrc/CalculateAverage_michaljonko.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_michaljonko.java
@@ -33,7 +33,6 @@ import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -58,7 +57,7 @@ public final class CalculateAverage_michaljonko {
 
     private static String sortedResults(Collection<StationMeasurement> results) {
         return results.stream()
-                .sorted(Comparator.comparing(stationMeasurement -> stationMeasurement.station.name()))
+                .sorted(Comparator.comparing(stationMeasurement -> stationMeasurement.station.name))
                 .map(StationMeasurement::data)
                 .collect(Collectors.joining(", ", "{", "}"));
     }
@@ -67,7 +66,7 @@ public final class CalculateAverage_michaljonko {
         try (var parseExecutorService = Executors.newFixedThreadPool(partitionsAmount);
                 var mergeExecutorService = Executors.newSingleThreadExecutor()) {
             final var memorySegments = FilePartitioner.createSegments(path, partitionsAmount);
-            final var parseFutures = new ArrayList<Future<HashMap<Integer, StationMeasurement>>>(memorySegments.size());
+            final var parseFutures = new ArrayList<Future<Collection<StationMeasurement>>>(memorySegments.size());
             final var results = new HashMap<Station, StationMeasurement>();
             for (var memorySegment : memorySegments) {
                 parseFutures.add(parseExecutorService.submit(() -> parse(memorySegment)));
@@ -76,7 +75,7 @@ public final class CalculateAverage_michaljonko {
             for (var future : parseFutures) {
                 final var futureResult = future.get();
                 lastFuture = mergeExecutorService.submit(
-                        () -> futureResult.forEach((k, v) -> results.merge(v.station, v, StationMeasurement::update)));
+                        () -> futureResult.forEach(v -> results.merge(v.station, v, StationMeasurement::update)));
             }
             lastFuture.get();
             return results.values();
@@ -86,58 +85,86 @@ public final class CalculateAverage_michaljonko {
         }
     }
 
-    private static HashMap<Integer, StationMeasurement> parse(MemorySegment memorySegment) {
+    private static Collection<StationMeasurement> parse(MemorySegment memorySegment) {
         final var stationNameRaw = new byte[MAX_STATION_NAME_LENGTH];
         final var temperatureRaw = new byte[MAX_TEMPERATURE_LENGTH];
         final var memorySegmentSize = memorySegment.byteSize();
-        final var stationsMap = new HashMap<Integer, StationMeasurement>(MAX_STATION_NAMES, 1f);
+        final var stationsMap = new HashMap<StationHash, StationMeasurement>(2 * MAX_STATION_NAMES);
 
         var stationNameRawIndex = 0;
-        var stationNameHash = 0;
+        var stationNameHashLow = 0;
+        var stationNameHashHigh = 97;
         var temperatureRawIndex = 0;
         var memorySegmentOffset = 0L;
 
         byte b;
         StationMeasurement stationMeasurement;
+        StationHash stationHash;
+        var temperature = 0;
 
         while (memorySegmentOffset < memorySegmentSize) {
             while ((b = memorySegment.get(ValueLayout.JAVA_BYTE, memorySegmentOffset++)) != ';') {
                 stationNameRaw[stationNameRawIndex++] = b;
-                stationNameHash = 31 * stationNameHash + b;
+                stationNameHashLow = (31 * stationNameHashLow + b);
+                stationNameHashHigh = (998551 * stationNameHashHigh + b);
             }
 
             while (memorySegmentOffset < memorySegmentSize && (b = memorySegment.get(ValueLayout.JAVA_BYTE, memorySegmentOffset++)) != '\n') {
                 temperatureRaw[temperatureRawIndex++] = b;
             }
+            temperature = TemperatureParser.parse(temperatureRaw, temperatureRawIndex);
 
-            final var temperature = TemperatureParser.parse(temperatureRaw, temperatureRawIndex);
-            stationMeasurement = stationsMap.get(stationNameHash);
+            stationHash = new StationHash(stationNameHashLow, stationNameHashHigh);
+            stationMeasurement = stationsMap.get(stationHash);
             if (stationMeasurement != null) {
                 stationMeasurement.update(temperature);
             }
             else {
-                stationsMap.put(stationNameHash, new StationMeasurement(new Station(stationNameRaw, stationNameRawIndex)).update(temperature));
+                stationsMap.put(stationHash, new StationMeasurement(new Station(stationNameRaw, stationNameRawIndex), temperature));
             }
 
             stationNameRawIndex = 0;
             temperatureRawIndex = 0;
-            stationNameHash = 0;
+            stationNameHashLow = 0;
+            stationNameHashHigh = 97;
         }
-        return stationsMap;
+        return stationsMap.values();
+    }
+
+    private static final class StationHash {
+        private final int low;
+        private final int high;
+
+        private StationHash(int low, int high) {
+            this.low = low;
+            this.high = high;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o)
+                return true;
+            final StationHash that = (StationHash) o;
+            return low == that.low && high == that.high;
+        }
+
+        @Override
+        public int hashCode() {
+            return high;
+        }
     }
 
     private static final class Station {
 
-        private static final ConcurrentHashMap<byte[], String> NAME_CACHE = new ConcurrentHashMap<>(MAX_STATION_NAMES);
         private final byte[] raw;
+        private final String name;
+        private final int hashCode;
 
         private Station(byte[] _raw, int length) {
             this.raw = new byte[length];
             System.arraycopy(_raw, 0, this.raw, 0, length);
-        }
-
-        public String name() {
-            return NAME_CACHE.computeIfAbsent(this.raw, k -> new String(k, 0, k.length));
+            this.name = new String(raw, 0, raw.length);
+            this.hashCode = Arrays.hashCode(raw);
         }
 
         @Override
@@ -145,7 +172,7 @@ public final class CalculateAverage_michaljonko {
             if (this == o) {
                 return true;
             }
-            if (o == null || getClass() != o.getClass()) {
+            if (o == null) {
                 return false;
             }
 
@@ -156,20 +183,23 @@ public final class CalculateAverage_michaljonko {
 
         @Override
         public int hashCode() {
-            return Arrays.hashCode(raw);
+            return hashCode;
         }
     }
 
     private static final class StationMeasurement {
 
         private final Station station;
-        private int min = Integer.MAX_VALUE;
-        private int max = Integer.MIN_VALUE;
-        private long count = 0L;
-        private long sum = 0L;
+        private int min;
+        private int max;
+        private long count;
+        private long sum;
 
-        private StationMeasurement(Station station) {
+        private StationMeasurement(Station station, int temperature) {
             this.station = station;
+            this.min = this.max = temperature;
+            this.sum = temperature;
+            this.count = 1;
         }
 
         private StationMeasurement update(int temperature) {
@@ -193,7 +223,7 @@ public final class CalculateAverage_michaljonko {
         }
 
         private String data() {
-            final var name = station.name();
+            final var name = station.name;
             final var min = this.min / 10.0d;
             final var avg = (1.0d * sum / count) / 10.0d;
             final var max = this.max / 10.0d;
@@ -211,24 +241,16 @@ public final class CalculateAverage_michaljonko {
 
             StationMeasurement that = (StationMeasurement) o;
 
-            if (min != that.min) {
-                return false;
-            }
-            if (max != that.max) {
-                return false;
-            }
-            if (count != that.count) {
-                return false;
-            }
-            if (sum != that.sum) {
-                return false;
-            }
-            return Objects.equals(station, that.station);
+            return min == that.min
+                    && max == that.max
+                    && count == that.count
+                    && sum == that.sum
+                    && Objects.equals(station, that.station);
         }
 
         @Override
         public int hashCode() {
-            return station != null ? station.hashCode() : 0;
+            return station.hashCode();
         }
     }
 
