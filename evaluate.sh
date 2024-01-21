38db7311a5b3f50cc99e5b9f4faa95b202157580
Gunnar Morling <gunnar.morling@googlemail.com>
2024-01-21 21:06:02 +0100

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -36,7 +36,7 @@ RESET='\033[0m' # No Color
 MEASUREMENTS_FILE="measurements_1B.txt"
 RUNS=5
 DEFAULT_JAVA_VERSION="21.0.1-open"
-BUILD_JAVA_VERSION="21.0.1-open"
+: "${BUILD_JAVA_VERSION:=21.0.1-open}"
 RUN_TIME_LIMIT=300 # seconds
 
 TIMEOUT=""
