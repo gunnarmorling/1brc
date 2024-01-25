406b3df20d39369f6deca2b1c93a03b84fa446cc
Diego <diegolparra@gmail.com>
2024-01-25 19:46:56 -0300

    use prepare.sh

--- calculate_average_dpsoft.sh
+++ calculate_average_dpsoft.sh
@@ -15,6 +15,6 @@
 #  limitations under the License.
 #
 
-JAVA_OPTS="--enable-preview -XX:-EnableJVMCI -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -Xms128m -Xmx128m -XX:+AlwaysPreTouch -XX:+UseTransparentHugePages -XX:-TieredCompilation"
+JAVA_OPTS="--enable-preview -XX:+UnlockExperimentalVMOptions -XX:-EnableJVMCI -XX:+UseEpsilonGC -Xms128m -Xmx128m -XX:+AlwaysPreTouch -XX:+UseTransparentHugePages -XX:-TieredCompilation"
 
 java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_dpsoft
