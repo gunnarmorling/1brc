b650e29198b542bcce25d1840bd011667927546a
Diego <diegolparra@gmail.com>
2024-01-25 21:56:40 -0300

    graal 21.0.2

--- prepare_dpsoft.sh
+++ prepare_dpsoft.sh
@@ -16,5 +16,5 @@
 #
 
 # Uncomment below to use sdk
- source "$HOME/.sdkman/bin/sdkman-init.sh"
- sdk use java 21.0.1-graal 1>&2
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 21.0.2-graal 1>&2
