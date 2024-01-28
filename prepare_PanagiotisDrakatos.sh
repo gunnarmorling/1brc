a6cd83fc9817de787591d27b1fe5d6527bb3aebd
PanosDR <PanagiotisDrakatos@users.noreply.github.com>
2024-01-28 11:25:53 +0200

    CalculateAverage_pdrakatos (#515)
    * CalculateAverage_pdrakatos
    
    * Rename to be valid with rules
    
    * CalculateAverage_pdrakatos
    
    * Rename to be valid with rules
    
    * Changes on scripts execution
    
    * Fixing bugs causing scripts not to be executed
    
    * Changes on prepare make it compatible
    
    * Fixing passing all tests
    
    * Increase direct memory allocation buffer
    
    * Fixing memory problem causes heap space exception

---
+++ prepare_PanagiotisDrakatos.sh
@@ -0,0 +1,23 @@
+#
+#  Copyright 2023 The original authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk use java 21.0.1-graal 1>&2
+
+if [ ! -f target/CalculateAverage_PanagiotisDrakatos_image ]; then
+    NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -R:MaxHeapSize=64m --initialize-at-build-time=dev.morling.onebrc.CalculateAverage_PanagiotisDrakatos"
+    native-image $NATIVE_IMAGE_OPTS -cp target/average-1.0.0-SNAPSHOT.jar -o target/CalculateAverage_PanagiotisDrakatos_image dev.morling.onebrc.CalculateAverage_PanagiotisDrakatos
+fi
