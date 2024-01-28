35120631e5bbfbdb1200df9f7b775e86947297cd
Guruprasad Sridharan <guruprasad.sridharan@gmail.com>
2024-01-29 00:13:07 +0530

    Minor fix

--- src/main/java/dev/morling/onebrc/CalculateAverage_godofwharf.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_godofwharf.java
@@ -250,7 +250,7 @@ public class CalculateAverage_godofwharf {
             int loopBound = PREFERRED_SPECIES.loopBound(pageLen);
             int i = 0;
             int j = 0;
-            int[] positions = new int[64];
+            //int[] positions = new int[64];
             while (j < loopBound) {
                 Vector<Byte> vec = ByteVector.fromArray(PREFERRED_SPECIES, page, j);
                 long res = NEW_LINE_VEC.eq(vec).toLong();
@@ -367,8 +367,8 @@ public class CalculateAverage_godofwharf {
         private static int[] computeHashCodes(final byte[] b) {
             // for perfect hashing, set seed as -2 and hash map size to 1<<14
             int[] res = new int[2];
-            res[0] = Arrays.hashCode(b);
-            //res[0] = hashCode1(b);
+            //res[0] = Arrays.hashCode(b);
+            res[0] = hashCode1(b);
             // res[1] = hashCode2(b);
             return res;
         }
@@ -419,13 +419,13 @@ public class CalculateAverage_godofwharf {
     }
 
     public static class State {
-        private final Map<AggregationKey, MeasurementAggregator> state;
-        //private final FastHashMap state;
+        //private final Map<AggregationKey, MeasurementAggregator> state;
+        private final FastHashMap state;
         // private final FastHashMap2 state;
 
         public State() {
-            this.state = new HashMap<>(DEFAULT_HASH_TBL_SIZE);
-            //this.state = new FastHashMap(1 << 14);
+            //this.state = new HashMap<>(DEFAULT_HASH_TBL_SIZE);
+            this.state = new FastHashMap(1 << 14);
             // this.state = new FastHashMap2(DEFAULT_HASH_TBL_SIZE);
         }
 
