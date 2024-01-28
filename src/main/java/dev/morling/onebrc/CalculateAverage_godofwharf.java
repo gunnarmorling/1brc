189fa775be3456a42852d8db803af1672638e149
Guruprasad Sridharan <guruprasad.sridharan@gmail.com>
2024-01-29 00:45:26 +0530

    Minor fix

--- src/main/java/dev/morling/onebrc/CalculateAverage_godofwharf.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_godofwharf.java
@@ -167,8 +167,8 @@ public class CalculateAverage_godofwharf {
                                     // Explicitly commented out because unload seems to take a lot of time
                                     // segment.unload();
                                 }
+                                printDebugMessage("Spent a total of %d ns in reading contents from file %n".formatted(timer));
                             }));
-                            printDebugMessage("Spent a total of %d ns in reading contents from file".formatted(time));
                         });
                 for (Future<?> future : futures) {
                     future.get();
