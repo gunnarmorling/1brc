936fc1da5493849d2aaf7f71f00f7f81067b6129
Alberto Venturini <aventurini@gmail.com>
2024-01-28 11:02:42 +0200

    Second version by albertoventurini (#609)
    * Contribution by albertoventurini
    
    * Use byte arrays of size 2^20
    
    ---------
    
    Co-authored-by: Alberto Venturini <alberto.venturini@accso.de>

--- calculate_average_albertoventurini.sh
+++ calculate_average_albertoventurini.sh
@@ -15,5 +15,5 @@
 #  limitations under the License.
 #
 
-JAVA_OPTS="-server -Xnoclassgc"
+JAVA_OPTS="-Xnoclassgc"
 java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_albertoventurini
