2b53c4de51a43fe06dab2f1f9d953125939417e5
dkarampi <dkarampin@gmail.com>
2024-01-28 17:25:38 +0100

    Formatted code

--- src/main/java/dev/morling/onebrc/CalculateAverage_dkarampi.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_dkarampi.java
@@ -34,23 +34,33 @@ public class CalculateAverage_dkarampi {
     private static final int BUFFER_SIZE = (1 << 29); // 500mb
     private static final int HT_SIZE = nextPowerOfTwo(10000);
     private static final int NUM_THREADS = 8;
-
-    private record Buffer(ByteBuffer byteBuffer, int length) {
-    }
-
-    private static class Station {
-        double sum;
-        double min = 100;
-        double max = -100;
-        int freq;
-        short nameLen;
-        byte[] name = new byte[nextPowerOfTwo(100)];
-    }
-
-    List<Station[]> stationHashTables = new ArrayList<>();
+    private final List<Station[]> stationHashTables = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         new CalculateAverage_dkarampi().runFast();
+    }
+
+    private static double round(double value) {
+        return Math.round(value * 10.0) / 10.0;
+    }
+
+    private static boolean areEqual(byte[] a, int aLen, byte[] b, int bLen) {
+        if (aLen != bLen) {
+            return false;
+        }
+        for (byte i = 0; i < aLen; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int nextPowerOfTwo(int n) {
+        for (int i = 1; i < 32; i <<= 1) {
+            n |= n >> i;
+        }
+        return n + 1;
     }
 
     private void runFast() throws Exception {
@@ -113,85 +123,6 @@ public class CalculateAverage_dkarampi {
         return buffers;
     }
 
-    private record Task(Station[] stations, List<Buffer> buffers) implements Runnable {
-
-        @Override
-        public void run() {
-            for (Buffer buffer : buffers) {
-                process(buffer);
-            }
-        }
-
-        private void process(Buffer buffer) {
-            short nameLen = 0;
-            int hash = 5381;
-            int temperature;
-            byte[] name = new byte[100];
-
-            for (int i = 0; i < buffer.length; i++) {
-                byte c = buffer.byteBuffer.get(i);
-                if (c == ';') {
-                    int sign = 1;
-                    c = buffer.byteBuffer.get(++i);
-                    if (c == '-') {
-                        sign = -1;
-                        c = buffer.byteBuffer.get(++i);
-                        temperature = (c - '0') * 10;
-                        c = buffer.byteBuffer.get(++i);
-                        if (c == '.') {
-                            c = buffer.byteBuffer.get(++i);
-                            temperature = temperature + c - '0';
-                        } else {
-                            temperature = temperature + c - '0';
-                            ++i; // dot
-                            c = buffer.byteBuffer.get(++i);
-                            temperature = temperature * 10 + c - '0';
-                        }
-                    } else {
-                        temperature = (c - '0') * 10;
-                        c = buffer.byteBuffer.get(++i);
-                        if (c == '.') {
-                            c = buffer.byteBuffer.get(++i);
-                            temperature = temperature + c - '0';
-                        } else {
-                            temperature = temperature + c - '0';
-                            ++i; // dot
-                            c = buffer.byteBuffer.get(++i);
-                            temperature = temperature * 10 + c - '0';
-                        }
-                    }
-                    hash = hash & 0x7FFFFFFF;
-                    updateStations(hash, name, nameLen, sign * (double) temperature / 10);
-                    ++i; // For '\n'
-                    nameLen = 0;
-                    hash = 5383;
-                } else {
-                    name[nameLen++] = c;
-                    hash = ((hash << 5) + hash) + c;
-                }
-            }
-        }
-
-        private void updateStations(int hash, byte[] name, short nameLen, double temperature) {
-            int idx;
-            for (idx = hash % HT_SIZE; stations[idx].freq != 0; idx = (idx + 1) % HT_SIZE) {
-                if (areEqual(stations[idx].name, stations[idx].nameLen, name, nameLen)) {
-                    stations[idx].sum += temperature;
-                    stations[idx].min = Math.min(stations[idx].min, temperature);
-                    stations[idx].max = Math.max(stations[idx].max, temperature);
-                    ++stations[idx].freq;
-                    return;
-                }
-            }
-            stations[idx].sum = temperature;
-            stations[idx].min = temperature;
-            stations[idx].max = temperature;
-            stations[idx].nameLen = nameLen;
-            System.arraycopy(name, 0, stations[idx].name, 0, nameLen);
-            stations[idx].freq = 1;
-        }
-    }
-
     private void sortAndPrint() {
         TreeMap<String, Station> sortedStations = new TreeMap<>();
 
@@ -233,26 +164,98 @@ public class CalculateAverage_dkarampi {
         System.out.println(sb);
     }
 
-    private static double round(double value) {
-        return Math.round(value * 10.0) / 10.0;
+    private record Buffer(ByteBuffer byteBuffer, int length) {
     }
 
-    private static boolean areEqual(byte[] a, int aLen, byte[] b, int bLen) {
-        if (aLen != bLen) {
-            return false;
-        }
-        for (byte i = 0; i < aLen; i++) {
-            if (a[i] != b[i]) {
-                return false;
+    private static class Station {
+        double sum;
+        double min = 100;
+        double max = -100;
+        int freq;
+        short nameLen;
+        byte[] name = new byte[nextPowerOfTwo(100)];
+    }
+
+    private record Task(Station[] stations, List<Buffer> buffers) implements Runnable {
+
+        @Override
+        public void run() {
+            for (Buffer buffer : buffers) {
+                process(buffer);
             }
         }
-        return true;
-    }
 
-    public static int nextPowerOfTwo(int n) {
-        for (int i = 1; i < 32; i <<= 1) {
-            n |= n >> i;
+        private void process(Buffer buffer) {
+            short nameLen = 0;
+            int hash = 5381;
+            int temperature;
+            byte[] name = new byte[100];
+
+            for (int i = 0; i < buffer.length; i++) {
+                byte c = buffer.byteBuffer.get(i);
+                if (c == ';') {
+                    int sign = 1;
+                    c = buffer.byteBuffer.get(++i);
+                    if (c == '-') {
+                        sign = -1;
+                        c = buffer.byteBuffer.get(++i);
+                        temperature = (c - '0') * 10;
+                        c = buffer.byteBuffer.get(++i);
+                        if (c == '.') {
+                            c = buffer.byteBuffer.get(++i);
+                            temperature = temperature + c - '0';
+                        }
+                        else {
+                            temperature = temperature + c - '0';
+                            ++i; // dot
+                            c = buffer.byteBuffer.get(++i);
+                            temperature = temperature * 10 + c - '0';
+                        }
+                    }
+                    else {
+                        temperature = (c - '0') * 10;
+                        c = buffer.byteBuffer.get(++i);
+                        if (c == '.') {
+                            c = buffer.byteBuffer.get(++i);
+                            temperature = temperature + c - '0';
+                        }
+                        else {
+                            temperature = temperature + c - '0';
+                            ++i; // dot
+                            c = buffer.byteBuffer.get(++i);
+                            temperature = temperature * 10 + c - '0';
+                        }
+                    }
+                    hash = hash & 0x7FFFFFFF;
+                    updateStations(hash, name, nameLen, sign * (double) temperature / 10);
+                    ++i; // For '\n'
+                    nameLen = 0;
+                    hash = 5383;
+                }
+                else {
+                    name[nameLen++] = c;
+                    hash = ((hash << 5) + hash) + c;
+                }
+            }
         }
-        return n + 1;
+
+        private void updateStations(int hash, byte[] name, short nameLen, double temperature) {
+            int idx;
+            for (idx = hash % HT_SIZE; stations[idx].freq != 0; idx = (idx + 1) % HT_SIZE) {
+                if (areEqual(stations[idx].name, stations[idx].nameLen, name, nameLen)) {
+                    stations[idx].sum += temperature;
+                    stations[idx].min = Math.min(stations[idx].min, temperature);
+                    stations[idx].max = Math.max(stations[idx].max, temperature);
+                    ++stations[idx].freq;
+                    return;
+                }
+            }
+            stations[idx].sum = temperature;
+            stations[idx].min = temperature;
+            stations[idx].max = temperature;
+            stations[idx].nameLen = nameLen;
+            System.arraycopy(name, 0, stations[idx].name, 0, nameLen);
+            stations[idx].freq = 1;
+        }
     }
 }
