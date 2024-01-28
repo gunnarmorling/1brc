3ed80c4b5f98b90a9ad871233638ef25a1c84442
Jaime Polidura <73758994+JaimePolidura@users.noreply.github.com>
2024-01-28 23:31:03 +0100

    Thank you for the challange!! (#599)
    * added code
    
    * Fixed pointers bugs
    
    * removed my own benchmark
    
    * added comment on how I handle hash collisions
    
    * executed mwvn clean verify
    
    * made scripts executable & fixed rounding issues
    
    * Fixed way of dealing with hash collisions
    
    * changed method name sameNameBytes to isSameNameBytes
    
    * changes script from sh to bash
    
    * fixed chunking bug
    
    * Fixed bug in chunking when file size is too small
    
    * added Runtime.getRuntime().availableProcessors

---
+++ calculate_average_JaimePolidura.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+#  Copyright 2023 The original authors
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+if [ -f target/CalculateAverage_JaimePolidura_image ]; then
+	target/CalculateAverage_JaimePolidura_image
+else
+	echo "Native image not found. Running in JVM mode"
+	JAVA_OPTS="--enable-preview -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:+UseTransparentHugePages -XX:+TrustFinalNonStaticFields"
+	java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_JaimePolidura
+fi
+
+
