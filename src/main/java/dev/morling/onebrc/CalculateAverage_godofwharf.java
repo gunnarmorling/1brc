06fafb222a254b48d85c6184e5961ec8ab0e64c2
Guruprasad Sridharan <guruprasad.sridharan@gmail.com>
2024-01-29 09:23:25 +0530

    Switch to normal map

--- src/main/java/dev/morling/onebrc/CalculateAverage_godofwharf.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_godofwharf.java
@@ -418,13 +418,13 @@ public class CalculateAverage_godofwharf {
     }
 
     public static class State {
-        // private final Map<AggregationKey, MeasurementAggregator> state;
-        private final FastHashMap state;
+        private final Map<AggregationKey, MeasurementAggregator> state;
+        //private final FastHashMap state;
         // private final FastHashMap2 state;
 
         public State() {
-            // this.state = new HashMap<>(DEFAULT_HASH_TBL_SIZE);
-            this.state = new FastHashMap(1 << 14);
+            this.state = new HashMap<>(DEFAULT_HASH_TBL_SIZE);
+            //this.state = new FastHashMap(1 << 14);
             // this.state = new FastHashMap2(DEFAULT_HASH_TBL_SIZE);
         }
 
