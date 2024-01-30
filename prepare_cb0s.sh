98c2dce7b997bf065d9e61620753c97ae11c7904
Cedric Boes <16071970+cb0s@users.noreply.github.com>
2024-01-30 15:20:46 +0100

    Update prepare_cb0s.sh

--- prepare_cb0s.sh
+++ prepare_cb0s.sh
@@ -16,13 +16,5 @@
 #
 
 # Uncomment below to use sdk
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk use java 21.0.1-graal 1>&2
-
-# used from thomaswue's prepare
-if [ ! -f target/CalculateAverage_cb0s_image ]; then
-    NATIVE_IMAGE_OPTS="--gc=epsilon -O3 -march=native --enable-preview"
-    # Use -H:MethodFilter=CalculateAverage_cb0s.* -H:Dump=:2 -H:PrintGraph=Network for IdealGraphVisualizer graph dumping.
-    native-image $NATIVE_IMAGE_OPTS -cp target/average-1.0.0-SNAPSHOT.jar -o target/CalculateAverage_cb0s_image dev.morling.onebrc.CalculateAverage_cb0s
-fi
-
+# source "$HOME/.sdkman/bin/sdkman-init.sh"
+# sdk use java 21.0.1-graal 1>&2
