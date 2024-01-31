7e7dd370a27dbcdef98bfa85678cd860def78fcc
Giedrius D <d.giedrius@gmail.com>
2024-01-31 12:55:34 +0000

    Adding java file to rise more eyebrows

--- calculate_average_gonix1st.sh
+++ calculate_average_gonix1st.sh
@@ -17,7 +17,7 @@
 
 
 JAVA_OPTS="--enable-preview"
-java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_gonix &
+java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_gonix1st &
 
 # Leaving some room for improvement
 sleep 0.1
