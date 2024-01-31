ceba9284313b28cfdb8eadcdfa9e885bedb8104b
mohammad.ashikur <mohammad.ashikur@neural-semiconductor.com>
2024-01-31 15:34:32 +0600

    added license header and the updated formatted code.

--- src/main/java/dev/morling/onebrc/CalculateAverage_ashikur2146.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_ashikur2146.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2023 The original authors
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package dev.morling.onebrc;
 
 import java.io.IOException;
@@ -6,7 +21,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
 public class CalculateAverage_ashikur2146 {
@@ -27,14 +41,14 @@ public class CalculateAverage_ashikur2146 {
     private static Map<String, TemperatureStats> processFile(String filePath) throws IOException {
         try {
             Path path = Paths.get(filePath);
-
-            ConcurrentMap<String, TemperatureStats> stationStats = Files.lines(path).parallel()
-                    .map(line -> line.split(";")).filter(parts -> parts.length == 2)
-                    .collect(Collectors.toConcurrentMap(parts -> parts[0],
-                            parts -> new TemperatureStats(Double.parseDouble(parts[1])), TemperatureStats::merge));
-
-            return new TreeMap<>(stationStats);
-
+            return Files.lines(path).map(line -> line.split(";")).parallel().filter(parts -> parts.length == 2)
+                    .collect(Collectors.groupingByConcurrent(parts -> parts[0],
+                            Collectors.mapping(parts -> new TemperatureStats(Double.parseDouble(parts[1])),
+                                    Collectors.reducing(TemperatureStats::merge))))
+                    .entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().orElseThrow(),
+                            (e1, e2) -> e1,
+                            TreeMap::new));
         }
         catch (IOException e) {
             throw new IOException("Error reading file: " + e.getMessage(), e);
