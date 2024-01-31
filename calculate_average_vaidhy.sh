67d1bb1f33cb6c151a773b144948fb1151d9936a
Anita SV <anitasvasu@gmail.com>
2024-01-31 12:20:44 -0800

    Remove native image from vaidhy

--- calculate_average_vaidhy.sh
+++ calculate_average_vaidhy.sh
@@ -15,11 +15,5 @@
 #  limitations under the License.
 #
 
-if [ -f target/CalculateAverage_vaidhy_image ]; then
-    echo "Picking up existing native image 'target/CalculateAverage_vaidhy_image', delete the file to select JVM mode." 1>&2
-    target/CalculateAverage_vaidhy_image
-else
-    JAVA_OPTS="--enable-preview"
-    echo "Chosing to run the app in JVM mode as no native image was found, use prepare_vaidhy.sh to generate." 1>&2
-    java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_vaidhy
-fi
+JAVA_OPTS="--enable-preview"
+java $JAVA_OPTS --class-path target/average-1.0.0-SNAPSHOT.jar dev.morling.onebrc.CalculateAverage_vaidhy
