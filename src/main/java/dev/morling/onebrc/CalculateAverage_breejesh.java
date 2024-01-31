fc5a0c313fb1eb0827a867a20477560fbb858df6
breejesh <breejesh2212@gmail.com>
2024-01-31 14:43:12 +0530

    Fix output

--- src/main/java/dev/morling/onebrc/CalculateAverage_breejesh.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_breejesh.java
@@ -15,10 +15,15 @@
  */
 package dev.morling.onebrc;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.nio.Buffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
 import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
@@ -69,65 +74,36 @@ public class CalculateAverage_breejesh {
     public static void main(String[] args) throws Exception {
         // long start = System.currentTimeMillis();
         // Find system details to determine cores and
-        final RandomAccessFile file = new RandomAccessFile(FILE, "r");
-        int cores = Runtime.getRuntime().availableProcessors();
+        var file = new File(args.length > 0 ? args[0] : FILE);
         long fileSize = file.length();
-        long splitSectionSize = fileSize / cores;
+        var numberOfCores = fileSize > 1_000_000 ? Runtime.getRuntime().availableProcessors() : 1;
+        var splitSectionSize = (int) Math.min(Integer.MAX_VALUE, fileSize / numberOfCores); // bytebuffer position is an int, so can be max Integer.MAX_VALUE
+        var segmentCount = (int) (fileSize / splitSectionSize);
 
-        // Divide file into segments equal to number of cores
-        MappedByteBuffer[] buffers = new MappedByteBuffer[cores];
-        for (int i = 0; i < cores; i++) {
+        // Divide file into segments
+        ExecutorService executor = Executors.newFixedThreadPool(segmentCount);
+        List<CompletableFuture<Map<String, Measurement>>> futures = new ArrayList<>();
+        for (int i = 0; i < segmentCount; i++) {
             long sectionStart = i * (long) splitSectionSize;
             long sectionEnd = Math.min(fileSize, sectionStart + splitSectionSize + 100);
-            buffers[i] = file.getChannel().map(FileChannel.MapMode.READ_ONLY, sectionStart, sectionEnd - sectionStart);
-        }
-
-        // Create thread pool
-        ExecutorService executor = Executors.newFixedThreadPool(cores);
-        List<CompletableFuture<Map<String, Measurement>>> futures = new ArrayList<>();
-        for (int j = 0; j < buffers.length; ++j) {
-            MappedByteBuffer currentBuffer = buffers[j];
-            int finalJ = j;
+            var fileChannel = (FileChannel) Files.newByteChannel(file.toPath(), StandardOpenOption.READ);
             CompletableFuture<Map<String, Measurement>> future = CompletableFuture.supplyAsync(() -> {
+                MappedByteBuffer currentBuffer = null;
+                try {
+                    currentBuffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, sectionStart, sectionEnd - sectionStart);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                // Skip till new line for unequal segments, not to be done for first section
+                if (sectionStart > 0) {
+                    while (currentBuffer.get() != '\n') ;
+                }
                 Map<String, Measurement> map = new HashMap<>();
                 while (currentBuffer.position() < splitSectionSize) {
-                    byte currentByte;
-                    var byteCounter = 0;
-                    var buffer = new byte[100];
-                    // skip half segment
-                    if (finalJ > 0) {
-                        while (currentBuffer.get() != '\n') ;
-                    }
-                    if(!currentBuffer.hasRemaining()) break;
-
-                    // read station
-                    while ((currentByte = currentBuffer.get()) != ';') {
-                        buffer[byteCounter++] = currentByte;
-                    }
-
-                    // read number
-                    int value;
-                    byte b1 = currentBuffer.get();
-                    byte b2 = currentBuffer.get();
-                    byte b3 = currentBuffer.get();
-                    byte b4 = currentBuffer.get();
-                    if (b2 == '.') {
-                        // case of n.n
-                        value = (b1 * 10 + b3 - TWO_BYTE_TO_INT);
-                    } else {
-                        if (b4 == '.') {
-                            // case of -nn.n
-                            value = -(b2 * 100 + b3 * 10 + currentBuffer.get() - THREE_BYTE_TO_INT);
-                        } else if (b1 == '-') {
-                            // case of -n.n
-                            value = -(b2 * 10 + b4 - TWO_BYTE_TO_INT);
-                        } else {
-                            // case of nn.n
-                            value = (b1 * 100 + b2 * 10 + b4 - THREE_BYTE_TO_INT);
-                        }
-                        currentBuffer.get(); // new line
-                    }
-                    String str = new String(buffer, 0, byteCounter, StandardCharsets.UTF_8);
+                    // Read station
+                    String str = getStationFromBuffer(currentBuffer);
+                    // Read number
+                    int value = getValueFromBuffer(currentBuffer);
                     if (map.containsKey(str)) {
                         map.get(str).append(value);
                     } else {
@@ -136,20 +112,59 @@ public class CalculateAverage_breejesh {
                 }
                 return map;
             }, executor);
-
             futures.add(future);
         }
 
         CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
-
         Map<String, Measurement> finalMap = new TreeMap<>();
         for (CompletableFuture<Map<String, Measurement>> future : futures) {
             Map<String, Measurement> map = future.get();
-            finalMap.putAll(map);
+            map.keySet().stream().forEach(
+                    key -> {
+                        if(finalMap.containsKey(key)) {
+                            finalMap.get(key).merge(map.get(key));
+                        } else {
+                            finalMap.put(key, map.get(key));
+                        }
+                    }
+            );
         }
 
         System.out.println(finalMap);
         // System.out.printf("Time %s", System.currentTimeMillis() - start);
         System.exit(0);
+    }
+
+    private static String getStationFromBuffer(MappedByteBuffer currentBuffer) {
+        byte currentByte;
+        var byteCounter = 0;
+        var buffer = new byte[100];
+        while ((currentByte = currentBuffer.get()) != ';') {
+            buffer[byteCounter++] = currentByte;
+        }
+        return new String(buffer, 0, byteCounter, StandardCharsets.UTF_8);
+    }
+
+    private static int getValueFromBuffer(MappedByteBuffer currentBuffer) {
+        int value;
+        byte [] nums = new byte[4];
+        currentBuffer.get(nums);
+        if (nums[1] == '.') {
+            // case of n.n
+            value = (nums[0] * 10 + nums[2] - TWO_BYTE_TO_INT);
+        } else {
+            if (nums[3] == '.') {
+                // case of -nn.n
+                value = -(nums[1] * 100 + nums[2] * 10 + currentBuffer.get() - THREE_BYTE_TO_INT);
+            } else if (nums[0] == '-') {
+                // case of -n.n
+                value = -(nums[1] * 10 + nums[3] - TWO_BYTE_TO_INT);
+            } else {
+                // case of nn.n
+                value = (nums[0] * 100 + nums[1] * 10 + nums[3] - THREE_BYTE_TO_INT);
+            }
+            currentBuffer.get(); // new line
+        }
+        return  value;
     }
 }
