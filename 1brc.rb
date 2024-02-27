5484c92beb8d1020ce405236e5ca50fe591d8c9f
Jacob Barrieault <jacob@pco.bz>
2024-02-27 14:30:21 -0500

    use unix time command instead of benchmark gem

--- 1brc.rb
+++ 1brc.rb
@@ -1,5 +1,3 @@
-require "benchmark"
-
 class NaiveBillionRowChallenge
   def self.call
     new.call
@@ -20,32 +18,19 @@ class NaiveBillionRowChallenge
       @results[city] ||= { min: nil, mean: nil, max: nil, sum: 0, count: 0 }
       data = @results[city]
 
-      begin
-        data[:min] = temp if data[:min].nil? || temp < data[:min]
-        data[:max] = temp if data[:max].nil? || temp > data[:max]
-        data[:count] += 1
-        data[:sum] += temp
-        data[:mean] = (data[:sum] / data[:count]).round(1)
-      rescue
-        # require 'pry'
-        # binding.pry
-        # exit
-      end
+      data[:min] = temp if data[:min].nil? || temp < data[:min]
+      data[:max] = temp if data[:max].nil? || temp > data[:max]
+      data[:count] += 1
+      data[:sum] += temp
 
       @results[city] = data
     end
 
     @results.each do |city, data|
-      puts "#{city}=#{data[:min]}/#{data[:mean]}/#{data[:max]}"
+      mean = (data[:sum] / data[:count]).round(1)
+      puts "#{city}=#{data[:min]}/#{mean}/#{data[:max]}"
     end
-    # output min/mean/max of each station to STDOUT
   end
-
-  private
 end
 
-time = Benchmark.measure do
-  NaiveBillionRowChallenge.call
-end
-
-puts "Completed in #{time} seconds"
+NaiveBillionRowChallenge.call
